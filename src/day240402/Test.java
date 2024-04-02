77493c32cd4b632fc033b37c57f782e879e58452
GuoXuanKun <1925918787@qq.com>
2024-04-02 15:30:38 +0800

    提交计算 `求支付金额` 的代码

--- src/day240402/Test.java
+++ src/day240402/Test.java
@@ -1,4 +1,18 @@
 package day240402;
 
 public class Test {
+    public static void main(String[] args) {
+        //总金额
+        int total = 10000;
+
+        //抵用券
+        double vouchers = 200.3;
+
+        //多少钱
+        double howMuch;
+
+        //计算需要支付的金额
+        howMuch = total - (((int)(((double)total - vouchers)/400)) *50) - vouchers;
+        System.out.println("需要支付的金额为:"+ howMuch);
+    }
 }
