3fc09f0c634c4c717cfa31a15b0a0a564f3d5f5a
GuoXuanKun <1925918787@qq.com>
2024-04-03 15:37:50 +0800

    submit TestPlusPlus

---
+++ src/day240403/TestPlusPlus.java
@@ -0,0 +1,13 @@
+package day240403;
+
+public class TestPlusPlus {
+    public static void main(String[] args) {
+        int a1 = 10;
+        int b1 = ++a1;
+        System.out.println("a1 = " + a1 + ", b1 = " + b1);
+
+        int a2 = 10;
+        int b2 = a2++;
+        System.out.println("a2 = " + a2 + ", b2 = " + b2);
+    }
+}
