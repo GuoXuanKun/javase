a1175f40306ce1a83e5fd906118d464fedfc4f13
GuoXuanKun <1925918787@qq.com>
2024-04-03 17:08:38 +0800

    update ForTest ...
    update:
    `\t`
    分行的位置

--- src/day240403/ForTest.java
+++ src/day240403/ForTest.java
@@ -4,15 +4,15 @@ public class ForTest {
     public static void main(String[] args) {
         //i行
         for (int i = 1; i < 10; i++) {
-            //分行
-            System.out.println();
-            //列
-            for (int j = 1; j <= i ; j++) {
 
-                    System.out.print( i + "*" + j + "=" + i*j + " ");
+            //列
+            for (int j = 1; j <= i; j++) {
+            // `\t`制表符
+                System.out.print(i + "*" + j + "=" + i * j + "\t");
 
             }
-
+            //分行
+            System.out.println();
         }
     }
 }
