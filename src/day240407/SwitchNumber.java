d261600d2c68cf3f2583bf6e9a2b0146ad09d02f
GuoXuanKun <1925918787@qq.com>
2024-04-07 11:37:37 +0800

    submit SwitchNumber

---
+++ src/day240407/SwitchNumber.java
@@ -0,0 +1,23 @@
+package day240407;
+
+public class SwitchNumber {
+    public static void main(String[] args) {
+        int x = 9;
+        int y = 8;
+        //需要的结果 x = 8 , y = 9
+
+        // method 1 , 直接赋值
+//        x = 8;
+//        y = 9;
+
+        // method 2 , 借助容器
+//        int temp;
+//        temp = x;
+//        x = y;
+//        y = temp;
+
+
+        System.out.println("x = "+ x);
+        System.out.println("y = "+ y);
+    }
+}
