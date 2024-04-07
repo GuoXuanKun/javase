208b3c96b5779a10b7c7f7ebfb45206b7fa59d2f
GuoXuanKun <1925918787@qq.com>
2024-04-07 13:52:06 +0800

    submit ScannerDemo

---
+++ src/day240407/ScannerDemo.java
@@ -0,0 +1,16 @@
+package day240407;
+
+import java.util.Scanner;
+
+public class ScannerDemo {
+    public static void main(String[] args) {
+        //
+        //Scanner
+        Scanner scanner = new Scanner(System.in);
+
+        //获取用户的输入
+        String msg = scanner.next();
+
+        System.out.println(msg);
+    }
+}
