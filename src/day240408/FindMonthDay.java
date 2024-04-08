04085960cb68b482b7882e45792b00a814b74e0b
GuoXuanKun <1925918787@qq.com>
2024-04-08 11:58:00 +0800

    submit FindMonthDay ...
    switch
    不考虑闰年的情况

--- src/day240408/FindMonthDay.java
+++ src/day240408/FindMonthDay.java
@@ -14,29 +14,44 @@ public class FindMonthDay {
         Scanner scanner = new Scanner(System.in);
         int month = scanner.nextInt();
 
-        //进行日期判断
-        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
-            System.out.println("本月天数为: 31" );
-        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
-            System.out.println("本月天数为: 30" );
-        }else {
-            //不考虑闰年的情况
-//            System.out.println("本月天数为: 28" );
+//        //进行日期判断
+//        if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+//            System.out.println("本月天数为: 31" );
+//        } else if (month == 4 || month == 6 || month == 9 || month == 11) {
+//            System.out.println("本月天数为: 30" );
+//        }else {
+//            //不考虑闰年的情况
+////            System.out.println("本月天数为: 28" );
+//
+//            //判断是否为闰年
+//            if(year % 4 == 0 ){
+//                if(year % 100 == 0 && year % 400 == 0){
+//                    System.out.println("本月天数为: 29" );
+//                }else if(year %100 != 0) {
+//                    System.out.println("本月天数为: 29" );
+//                }else{
+//                    System.out.println("本月天数为: 28" );
+//                }
+//            }else {
+//                System.out.println("本月天数为: 28" );
+//            }
+//
+//        }
 
-            //判断是否为闰年
-            if(year % 4 == 0 ){
-                if(year % 100 == 0 && year % 400 == 0){
-                    System.out.println("本月天数为: 29" );
-                }else if(year %100 != 0) {
-                    System.out.println("本月天数为: 29" );
-                }else{
-                    System.out.println("本月天数为: 28" );
-                }
-            }else {
-                System.out.println("本月天数为: 28" );
-            }
+        int day;
+        switch (month){
+            case 1,3,5,7,8,10,12:
+                day = 31;
+                break;
 
+            case 4,6,9,11:
+                day = 30;
+                break;
+
+            case 2:
+                day = 28;
+                break;
         }
-
+        System.out.println();
     }
 }
