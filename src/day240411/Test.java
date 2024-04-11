a736442e33cbca0a996e219216ea323b578d538f
GuoXuanKun <1925918787@qq.com>
2024-04-11 11:29:47 +0800

    草稿

--- src/day240411/Test.java
+++ src/day240411/Test.java
@@ -1,4 +1,21 @@
 package day240411;
 
 public class Test {
+    // 在特定的数字 后 + 编码 (可以是摩尔斯电码,或是其他)
+    //表示学生
+    int category = 100000;
+
+    //表示姓名
+    int name = 100010;
+
+    //表示性别
+    int sex = 100100;
+
+    //表示发型
+    int faxing = 101000;
+
+    //表示年龄
+
+    int age = 110000;
+
 }
