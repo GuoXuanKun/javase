a7d4f099d3b4d81bc0754cb290e32e697b578f25
GuoXuanKun <1925918787@qq.com>
2024-04-15 11:10:20 +0800

    submit Practice Stuent Class ...
    冒泡排序 学生成绩

---
+++ src/day240415/practice/Student.java
@@ -0,0 +1,26 @@
+package day240415.practice;
+
+public class Student {
+    // state
+    int number;
+    int state;
+    int score;
+
+    // behavior
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    public void setState(int state) {
+        this.state = state;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    // constructor
+    public Student(){
+
+    }
+}
