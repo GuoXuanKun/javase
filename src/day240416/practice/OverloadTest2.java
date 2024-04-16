e9f854c46cab88c942c20e7633bb376a56981585
GuoXuanKun <1925918787@qq.com>
2024-04-16 11:02:13 +0800

    commit OverloadTest2 ...
    println 重载

---
+++ src/day240416/practice/OverloadTest2.java
@@ -0,0 +1,17 @@
+package day240416.practice;
+
+import org.w3c.dom.ls.LSOutput;
+
+public class OverloadTest2 {
+    public static void main(String[] args) {
+
+        int[] arr1 = new int[]{1,2,3};
+        System.out.println(arr1);
+
+        char[] arr2 = new char[]{'a','b','c','d','e'};
+        System.out.println(arr2);
+
+        boolean[] arr3 = new boolean[]{false,true,true};
+        System.out.println(arr3);
+    }
+}
