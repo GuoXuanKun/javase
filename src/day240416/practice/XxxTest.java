dc41d722633d568842e88c7a093cec33fc9a97e8
GuoXuanKun <1925918787@qq.com>
2024-04-16 16:06:45 +0800

    commit 需求1 ...
    计算两个 int 数之和

---
+++ src/day240416/practice/XxxTest.java
@@ -0,0 +1,21 @@
+package day240416.practice;
+
+public class XxxTest {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+
+
+    // 需求3: 你的代码
+
+    public static void main(String[] args) {
+        // 需求1: 计算两个 int 数之和
+        System.out.println(add(1, 2));
+
+        // 需求2: 计算三个 int 数之和
+
+
+        // 需求3: 计算 n 个 int 数之和。不确定个数，你会怎么写？
+    }
+}
