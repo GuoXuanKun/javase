19b2311cb2cdd8224ed32b585f582e5d004741a7
GuoXuanKun <1925918787@qq.com>
2024-04-17 15:06:12 +0800

    VarargsDetail

---
+++ src/day240417/practice/VarargsDetail.java
@@ -0,0 +1,15 @@
+package day240417.practice;
+
+public class VarargsDetail {
+    public static void main(String[] args) {
+
+//    public static void demo1(int a) {}
+//    public static void demo1(int[] is) {}
+//    public static void demo1(int... is) {}
+//    public static void demo2(int type, int c, int... is) {}
+//    public static void demo3(int... is, int type, int c) {}
+//    public static void demo4(String type, char c, int... is) {}
+//    public static void demo5(int... is, String type, char c) {}
+//    public static void demo6(int... is1, int... is2) {}
+    }
+}
