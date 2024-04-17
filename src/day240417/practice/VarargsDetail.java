78f47026696ebeceb17b61e6f1ac1c9fba09048e
GuoXuanKun <1925918787@qq.com>
2024-04-17 16:22:34 +0800

    VarargsDetail int... = int[]

--- src/day240417/practice/VarargsDetail.java
+++ src/day240417/practice/VarargsDetail.java
@@ -2,33 +2,49 @@ package day240417.practice;
 
 public class VarargsDetail {
     public static void main(String[] args) {
-
-        // personal: 不会出错
-//    public static void demo1(int a) {}
-
-        // personal: 不会出错
-//    public static void demo1(int[] is) {}
-
-        // personal: 不会出错
-//    public static void demo1(int... is) {}
-
-        // personal: 会出错
-        // 变长数组不能放在后面
-//    public static void demo2(int type, int c, int... is) {}
-
-        // personal: 会出错
-        // 变长数组不能放在后面
-//    public static void demo3(int... is, int type, int c) {}
-
-        // personal: 会出错
-        // 变长数组不能放在后面
-//    public static void demo4(String type, char c, int... is) {}
-
-        // personal: 不会出错
-//    public static void demo5(int... is, String type, char c) {}
-
-        // personal: 会出错
-        // 变长数组不能放在后面
-//    public static void demo6(int... is1, int... is2) {}
     }
+
+    // personal: 不会出错
+    public static void demo1(int a) {
+    }
+
+    // personal: 不会出错
+//        public static void demo1 ( int[] is){
+//        }
+
+    // personal: 不会出错
+    // reality: int... 的本质,或者说JVM底层就是把它当成数组来处理的
+    // 跟上一个方法的签名一致, 就是重名了 , 所以会报错
+//        public static void demo1 ( int...is){
+//        }
+
+    // personal: 会出错
+    // reality: 不会出错
+    //
+//        public static void demo2 ( int type, int c, int...is){
+//        }
+
+    // personal: 会出错
+    // 变长数组不能放在前面
+
+//        public static void demo3 ( int...is,int type, int c){
+//        }
+
+    // personal: 会出错
+    // reality: 不会出错
+//        public static void demo4 (String type,char c, int...is){
+//        }
+
+    // personal: 不会出错
+    // reality: 会出错
+
+//        public static void demo5 ( int...is, String type,char c){
+//        }
+
+    // personal: 会出错
+    // 变长数组不能放在前面
+
+//        public static void demo6 ( int...is1,int...is2){
+//        }
 }
+
