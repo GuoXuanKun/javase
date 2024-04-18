5782ce6ad434cdd44ebbca4f93df24342d00121c
GuoXuanKun <1925918787@qq.com>
2024-04-18 09:56:37 +0800

    Student Class step 1:...
    Student 类可以 被实例化

---
+++ src/day240418/practice/Student.java
@@ -0,0 +1,22 @@
+package day240418.practice;
+
+public class Student {
+    // 1. state
+    int id;
+    String name;
+
+    // 3. constructor
+    public Student(){
+
+    }
+
+    // 2. behavior
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
