83ed9fef269de4bcd653bdb5bca3a0cd7d0a8f19
GuoXuanKun <1925918787@qq.com>
2024-04-22 22:54:19 +0800

    JsoupDemo3 ...
    1.获取每个标签的文本
    2.获取链接
    3.获取 含有苹果的文本 以及 对应的链接

---
+++ src/day240422/practice/JsoupDemo3.java
@@ -0,0 +1,56 @@
+package day240422.practice;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class JsoupDemo3 {
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("http://bbs.xmfish.com/thread-htm-fid-55.html").get();
+
+        // 获取所有链接 但是有部分不对
+//        System.out.println(doc.select("td[class=subject] > a[class=subject_t f14]").text());
+
+        String theText = "[闲置转让]"; // 需要查找的文本
+
+
+        // 查找包含指定文本的第一个a标签
+//        Element linkWithText = doc.select("a:containsOwn(" + theText + ")").first();
+        // 获取该a标签的下一个兄弟元素（a标签）  注意: nextElementSibling() 方法返回的是下一个元素，不一定是 <a> 标签
+//        Element nextLink = linkWithText.nextElementSibling();
+        // 打印出来
+//        System.out.println("Link with text: " + linkWithText.text());
+//        System.out.println("Next link: " + nextLink.text());
+
+
+        // 查找所有包含指定文本的a标签
+        // Elements 类表示一组Element
+//        Elements linksWithText = doc.select("a:containsOwn(" + theText + ")");
+//        for (Element linkWithText : linksWithText) {
+//            // 获取该a标签的兄弟元素（下一个a标签）  注意: nextElementSibling() 方法返回的是下一个元素，不一定是 <a> 标签
+//            Element nextLink = linkWithText.nextElementSibling();
+//            // 打印 文本 并且含 地址
+//            System.out.println("Link with text: " + linkWithText.text() + ", Next link: " + nextLink.text() + ", Next link URL: " + nextLink.attr("href"));
+//        }
+
+        System.out.println("------------------------------------------------------------------------------------------------------------------------------");
+
+        // 查找所有包含指定文本的a标签
+        // Elements 类表示一组Element
+        Elements linksWithText = doc.select("a:containsOwn(" + theText + ")");
+        for (Element linkWithText : linksWithText) {
+            // 获取该a标签的兄弟元素（下一个a标签）  注意: nextElementSibling() 方法返回的是下一个元素，不一定是 <a> 标签
+            Element nextLink = linkWithText.nextElementSibling();
+            // 打印 含"苹果"的文本 并且含 地址
+            if (nextLink != null && "a".equals(nextLink.tagName()) && nextLink.text().contains("苹果")) {
+                System.out.println("Link with text: " + linkWithText.text() + ", Next link :" + nextLink.text() + "' URL: " + nextLink.attr("href"));
+            }
+        }
+
+
+    }
+
+}
