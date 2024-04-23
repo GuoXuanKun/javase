c66833c8a3a9e5d5879076e0cb06c10e688da154
GuoXuanKun <1925918787@qq.com>
2024-04-23 09:32:24 +0800

    JsoupDemo3 ...
    1.获取每个标签的文本
    2.获取链接
    3.获取 含有苹果的文本 以及 对应的链接
    完善细节

--- src/day240422/practice/JsoupDemo3.java
+++ src/day240422/practice/JsoupDemo3.java
@@ -28,25 +28,25 @@ public class JsoupDemo3 {
 
         // 查找所有包含指定文本的a标签
         // Elements 类表示一组Element
-//        Elements linksWithText = doc.select("a:containsOwn(" + theText + ")");
-//        for (Element linkWithText : linksWithText) {
-//            // 获取该a标签的兄弟元素（下一个a标签）  注意: nextElementSibling() 方法返回的是下一个元素，不一定是 <a> 标签
-//            Element nextLink = linkWithText.nextElementSibling();
-//            // 打印 文本 并且含 地址
-//            System.out.println("Link with text: " + linkWithText.text() + ", Next link: " + nextLink.text() + ", Next link URL: " + nextLink.attr("href"));
-//        }
+        Elements linksWithText = doc.select("a:containsOwn(" + theText + ")");
+        for (Element linkWithText : linksWithText) {
+            // 获取该a标签的兄弟元素（下一个a标签）  注意: nextElementSibling() 方法返回的是下一个元素，不一定是 <a> 标签
+            Element nextLink = linkWithText.nextElementSibling();
+            // 打印 文本 并且含 地址
+            System.out.println("Link with text: " + linkWithText.text() + ", Next link: " + nextLink.text() + ", Next link URL: " +"http://bbs.xmfish.com/" +nextLink.attr("href"));
+        }
 
         System.out.println("------------------------------------------------------------------------------------------------------------------------------");
 
         // 查找所有包含指定文本的a标签
         // Elements 类表示一组Element
-        Elements linksWithText = doc.select("a:containsOwn(" + theText + ")");
-        for (Element linkWithText : linksWithText) {
+        Elements linksWithText0 = doc.select("a:containsOwn(" + theText + ")");
+        for (Element linkWithText : linksWithText0) {
             // 获取该a标签的兄弟元素（下一个a标签）  注意: nextElementSibling() 方法返回的是下一个元素，不一定是 <a> 标签
             Element nextLink = linkWithText.nextElementSibling();
             // 打印 含"苹果"的文本 并且含 地址
             if (nextLink != null && "a".equals(nextLink.tagName()) && nextLink.text().contains("苹果")) {
-                System.out.println("Link with text: " + linkWithText.text() + ", Next link :" + nextLink.text() + "' URL: " + nextLink.attr("href"));
+                System.out.println("Link with text: " + linkWithText.text() + ", Next link :" + nextLink.text() + "' URL: " + "http://bbs.xmfish.com/" +nextLink.attr("href"));
             }
         }
 
