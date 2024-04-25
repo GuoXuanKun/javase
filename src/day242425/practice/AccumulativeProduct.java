74b122cac33b0d55a18955f2f2511bf2103cf447
GuoXuanKun <1925918787@qq.com>
2024-04-25 16:59:45 +0800

    计算随机数的阶乘 (这个版本不能输入太大的值)

---
+++ src/day242425/practice/AccumulativeProduct.java
@@ -0,0 +1,10 @@
+package day242425.practice;
+
+public class AccumulativeProduct {
+    public int multiply(int n) {
+        if (n == 1) {
+            return n;
+        }
+        return n * multiply(n - 1);
+    }
+}
---
+++ src/day242425/practice/AccumulativeProductTest.java
@@ -0,0 +1,13 @@
+package day242425.practice;
+
+import java.util.Scanner;
+
+public class AccumulativeProductTest {
+    public static void main(String[] args) {
+        AccumulativeProduct accumulativeProduct = new AccumulativeProduct();
+        System.out.println("请输入要算阶乘的数");
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println(accumulativeProduct.multiply(n));
+    }
+}
