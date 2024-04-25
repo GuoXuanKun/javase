46cae9a7787aab80c887d3c6724b1e3412e15b8e
GuoXuanKun <1925918787@qq.com>
2024-04-25 16:17:33 +0800

    斐波那契 Debug

---
+++ src/day242425/practice/Fibonacci.java
@@ -0,0 +1,19 @@
+package day242425.practice;
+
+public class Fibonacci {
+    // 需要思考的点：int 类型够不够用？
+    public int f(int n) {
+        // 重点：这个方法一定要有出口。
+        // 或者说要有出栈的机会，不然就会出现 StackOverflowError
+        System.out.println("n=" + n);
+        int value;
+        if (n == 1 || n == 2) {
+            value = 1;
+            System.out.println("n=" + n + ", value=" + 1);
+            return value;
+        }
+        value = f(n - 1) + f(n - 2);
+        System.out.println("n=" + n + ", value=" + value);
+        return value;
+    }
+}
---
+++ src/day242425/practice/FibonacciTestDebug.java
@@ -0,0 +1,9 @@
+package day242425.practice;
+
+public class FibonacciTestDebug {
+    public static void main(String[] args) {
+        Fibonacci fibonacci = new Fibonacci();
+        int n = 6;
+        System.out.println("第 " + n + " 位斐波那契数列的值=" + fibonacci.f(n));
+    }
+}
