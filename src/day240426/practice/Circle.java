5830c6a468756cb6b8c091d0ad596ebd7bb86a00
GuoXuanKun <1925918787@qq.com>
2024-04-26 15:18:28 +0800

    Circle step 1 基础版

---
+++ src/day240426/practice/Circle.java
@@ -0,0 +1,24 @@
+package day240426.practice;
+
+public class Circle {
+
+    // 1. state (fields , variable)
+    public double r = 1.5;
+    public double pi = 3.14;
+
+    public double area;
+
+    // 2. behavior ( method , function)
+
+    public double getArea() {
+        area = r * r * pi;
+        return area;
+    }
+
+    public void setArea(double area) {
+        this.area = area;
+    }
+
+
+    // 3. constructor
+}
