fa014ef5fbfd5f5c9a3ab10c430770a62b7d1d97
GuoXuanKun <1925918787@qq.com>
2024-04-26 16:58:54 +0800

    重新认识 toString()

---
+++ src/day240426/practice/Boy.java
@@ -0,0 +1,16 @@
+package day240426.practice;
+
+public class Boy extends Person{
+
+    // 3. constructor
+    public Boy() {
+    }
+
+    @Override
+    public String toString() {
+        return "Boy{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
