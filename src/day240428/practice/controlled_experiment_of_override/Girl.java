091c52628f5db1c80f166ca44302e30a70ec5f5f
GuoXuanKun <1925918787@qq.com>
2024-04-28 15:35:21 +0800

    controlled experiment of override   重写的对照实验 2 (更换访问权限修饰符 为 no modified) ...
    报错:内容为 正在尝试分配更低的访问权限; 以前为public

--- src/day240428/practice/controlled_experiment_of_override/Girl.java
+++ src/day240428/practice/controlled_experiment_of_override/Girl.java
@@ -2,7 +2,13 @@ package day240428.practice.controlled_experiment_of_override;
 
 public class Girl extends Person{
     @Override
-    public void eat() {
-        System.out.println("女生也要吃饭");
+//    public void eat() {
+//        System.out.println("女生也要吃饭");
+//    }
+
+    // 更改 访问权限修饰符
+    // 报错内容为: 正在尝试分配更低的访问权限; 以前为public
+     void eat(){
+        System.out.println("女生也要吃饭 no modified");
     }
 }
