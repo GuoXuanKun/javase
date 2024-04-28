f40e8e38ef98940f984f14b9c9d75e56bda07236
GuoXuanKun <1925918787@qq.com>
2024-04-28 16:31:17 +0800

    controlled experiment of override   重写的对照实验 ( version 2 ) 2.1 方法名 ( 形参列表 types ) ...
    父类被重写的方法与子类重写的方法的方法名和形参列表 types 必须相同

--- src/day240428/practice/controlled_experiment_of_override/Girl.java
+++ src/day240428/practice/controlled_experiment_of_override/Girl.java
@@ -2,9 +2,9 @@ package day240428.practice.controlled_experiment_of_override;
 
 public class Girl extends Person{
     @Override
-    public void eat() {
-        System.out.println("女生也要吃饭");
-    }
+//    public void eat() {
+//        System.out.println("女生也要吃饭");
+//    }
 
 //    // 更改 访问权限修饰符
 //    // 报错 内容为: 正在尝试分配更低的访问权限; 以前为public
@@ -33,4 +33,9 @@ public class Girl extends Person{
 //        public void eat() {
 //        System.out.println("女生当然也要吃饭");
 //    }
+    // 方法名 (形参列表)
+        public void eat() {
+//        System.out.println("女生也要吃饭");
+    }
+
 }
