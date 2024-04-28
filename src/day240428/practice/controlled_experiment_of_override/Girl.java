900b9cb3cdb4dc6bf643b54f8cc16171ee7b4ed0
GuoXuanKun <1925918787@qq.com>
2024-04-28 15:53:48 +0800

    controlled experiment of override   重写的对照实验 7 实验报告 自己做的 还不够细致 参考最新版本...
    重写要起作用 要有以下因素
    ( 访问权限修饰符 返回值类型 方法名 参数列表 ) 必须与父类的一样

--- src/day240428/practice/controlled_experiment_of_override/Girl.java
+++ src/day240428/practice/controlled_experiment_of_override/Girl.java
@@ -2,35 +2,35 @@ package day240428.practice.controlled_experiment_of_override;
 
 public class Girl extends Person{
     @Override
-//    public void eat() {
-//        System.out.println("女生也要吃饭");
-//    }
+    public void eat() {
+        System.out.println("女生也要吃饭");
+    }
 
-    // 更改 访问权限修饰符
-    // 报错 内容为: 正在尝试分配更低的访问权限; 以前为public
+//    // 更改 访问权限修饰符
+//    // 报错 内容为: 正在尝试分配更低的访问权限; 以前为public
 //     void eat(){
 //        System.out.println("女生也要吃饭 no modified");
 //    }
-
-    // 更改返回类型
-    // 报错 内容为: attempting to use incompatible return type 返回类型java.lang.String与void不兼容
+//
+//    // 更改返回类型
+//    // 报错 内容为: attempting to use incompatible return type 返回类型java.lang.String与void不兼容
 //        public String eat() {
 //        System.out.println("女生也要吃饭");
 //        return "女生也要吃饭";
 //    }
-
-    // 更改方法名
+//
+//    // 更改方法名
 //    public void eae(){
 //        System.out.println("女生也要吃饭");
 //    }
-
-    // 更改参数类型列表
+//
+//    // 更改参数类型列表
 //    public void eat(int a){
 //        System.out.println("女生也要吃饭");
 //    }
-
-    // 更改方法体
-        public void eat() {
-        System.out.println("女生当然也要吃饭");
-    }
+//
+//    // 更改方法体
+//        public void eat() {
+//        System.out.println("女生当然也要吃饭");
+//    }
 }
