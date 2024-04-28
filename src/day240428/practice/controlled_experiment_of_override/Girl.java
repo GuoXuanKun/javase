c471cb71575e5bfbf968fa49d4d7bf71ad7d6233
GuoXuanKun <1925918787@qq.com>
2024-04-28 15:41:10 +0800

    controlled experiment of override   重写的对照实验 4 (更换方法名 为 eae) ...
    无报错
    但是没有重写

--- src/day240428/practice/controlled_experiment_of_override/Girl.java
+++ src/day240428/practice/controlled_experiment_of_override/Girl.java
@@ -14,8 +14,13 @@ public class Girl extends Person{
 
     // 更改返回类型
     // 报错 内容为: attempting to use incompatible return type 返回类型java.lang.String与void不兼容
-        public String eat() {
+//        public String eat() {
+//        System.out.println("女生也要吃饭");
+//        return "女生也要吃饭";
+//    }
+
+    // 更改方法名
+    public void eae(){
         System.out.println("女生也要吃饭");
-        return "女生也要吃饭";
     }
 }
