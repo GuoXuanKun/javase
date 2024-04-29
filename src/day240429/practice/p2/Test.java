1c2b448f3d181a789e98e926e89f20cec40be3fa
GuoXuanKun <1925918787@qq.com>
2024-04-29 15:06:39 +0800

    练习2 : Overriding it - 思考 ...
    // 思考:
    // Animal animal = cat;
    引出直接打印 char[] 竟然不是我们预想的 '地址值' : 19fcf5cf

--- src/day240429/practice/p2/Test.java
+++ src/day240429/practice/p2/Test.java
@@ -3,6 +3,9 @@ package day240429.practice.p2;
 public class Test {
     public static void main(String[] args) {
         Cat cat = new Cat();
-
+        // 思考:
+        // Animal animal = cat;
+        // 引出直接打印 char[] 竟然不是我们预想的 '地址值'
+        Animal animal = cat;
     }
 }
