deb5e62c5f4d90efac34345143edc70ffd98c4e2
GuoXuanKun <1925918787@qq.com>
2024-04-29 20:50:44 +0800

    super 访问构造器 - 基础代码

---
+++ src/day240429/practice/verify/Person.java
@@ -0,0 +1,29 @@
+package day240429.practice.verify;
+
+public class Person {
+
+    // state
+    String id;
+    String name;
+    int age;
+
+    // behavior
+    public Person() {
+    }
+
+    public Person(String id) {
+        this.id = id;
+    }
+
+    public Person(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Person(String id, int age, String name) {
+        this.id = id;
+        this.age = age;
+        this.name = name;
+    }
+
+}
---
+++ src/day240429/practice/verify/Student.java
@@ -0,0 +1,12 @@
+package day240429.practice.verify;
+
+public class Student extends Person{
+   // state
+    String school;
+
+    // behavior
+    public Student(String school) {
+        this.school = school;
+    }
+
+}
