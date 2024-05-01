4778f86dafaeb08e732e7baaa01ce127ee56d0ca
GuoXuanKun <1925918787@qq.com>
2024-05-01 21:42:09 +0800

    漫画 - 场景1 - 引出 abstract 关键字。品它，后续再详细说

--- src/day240430/practice/xxx/Person.java
+++ src/day240430/practice/xxx/Person.java
@@ -1,11 +1,9 @@
 package day240430.practice.xxx;
 
-public class Person {
+public abstract class Person {
     // state
     String name;
 
     // behavior
-    public void describeLonely(String text) {
-        System.out.println("我可以描述，也可以不描述，因为我知道从我继承的每个人都有自己的孤独");
-    }
+    public abstract void describeLonely(String text) ;
 }
