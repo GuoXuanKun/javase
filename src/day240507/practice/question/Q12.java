0b1e98f8d7575e0480055eb9acaa104ee99f146d
GuoXuanKun <1925918787@qq.com>
2024-05-07 23:00:54 +0800

    Q12. 如何确保以下的 Student 类，有且仅有一个实例

---
+++ src/day240507/practice/question/Q12.java
@@ -0,0 +1,9 @@
+package day240507.practice.question;
+
+//如何确保以下的 Student 类，有且仅有一个实例
+public class Q12 {
+    public static void main(String[] args) {
+        // 从提交记录中搜索 `单例`
+        // day240418.p1.Student
+    }
+}
