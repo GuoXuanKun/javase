470bcc03a99fcdf0b13278beba9162e86bd6e97b
GuoXuanKun <1925918787@qq.com>
2024-05-07 23:00:22 +0800

    Q10: ...
    10. 如何给 xx 和 yy 赋值

---
+++ src/day240507/practice/question/Q10.java
@@ -0,0 +1,10 @@
+package day240507.practice.question;
+
+//10. 如何给 xx 和 yy 赋值
+public class Q10 {
+    public static void main(String[] args) {
+        int[] xx, yy[];
+        // xx = 你的代码;
+        // yy = 你的代码;
+    }
+}
