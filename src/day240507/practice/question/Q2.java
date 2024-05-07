6e5c843512735c0f49270ef66128dc14c5c9622e
GuoXuanKun <1925918787@qq.com>
2024-05-07 16:59:14 +0800

    Q2 Java 的文档注释的格式是什么?

---
+++ src/day240507/practice/question/Q2.java
@@ -0,0 +1,11 @@
+package day240507.practice.question;
+
+/**
+ * 2. Java 文档注释的格式是什么？
+ */
+public class Q2 {
+    public static void main(String[] args) {
+        // 什么时候使用
+        System.out.println("使用 javadoc 命令 会生成一个 API 文件(网页)");
+    }
+}
