a58c60a9954e3cd6a36e54d056bb4638793e55a4
GuoXuanKun <1925918787@qq.com>
2024-05-07 17:01:29 +0800

    Q3 定义变量可以以 `$` 符号开头，但是不建议

---
+++ src/day240507/practice/question/Q3.java
@@ -0,0 +1,12 @@
+package day240507.practice.question;
+
+/**
+ * 3. 下列语句会报错吗，为什么？
+ */
+public class Q3 {
+    public static void main(String[] args) {
+        int $j = 2;
+        // 不会报错。
+        // 可以以 `$` 符号开头，但是不建议
+    }
+}
