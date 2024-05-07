c494d161b1f84f35b57499c7a50f9560f3d64125
GuoXuanKun <1925918787@qq.com>
2024-05-07 23:00:37 +0800

    Q11. 以下选项会报错的是？

---
+++ src/day240507/practice/question/Q11.java
@@ -0,0 +1,25 @@
+package day240507.practice.question;
+
+//11. 以下选项会报错的是？
+public class Q11 {
+    public static void main(String[] args) {
+        int[] xx, yy[];
+        // 前提：
+        // 假设已经给 xx 和 yy 赋值了
+
+        // xx = 你的代码
+        // yy = 你的代码
+
+        // 问：
+        // 在以下 A 到 F 的选项中，打开注释之后哪些不会报错？
+
+        /*
+        xx[0] = yy;        // A
+        yy[0] = xx;        // B √
+        yy[0][0] = xx;     // C
+        xx[0][0] = yy;     // D
+        yy[0][0] = xx[0];  // E √
+        xx = yy;           // F
+        */
+    }
+}
