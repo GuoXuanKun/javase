9d72f99714e7bd7cc82bc2edfebd238a0636a4c3
GuoXuanKun <1925918787@qq.com>
2024-05-07 16:43:20 +0800

    Q1 手写 HelloWorld

---
+++ src/day240507/practice/question/Q1.java
@@ -0,0 +1,8 @@
+package day240507.practice.question;
+
+public class Q1 {
+    // 打印HelloWorld 手写 ,不用快捷键
+    public static void main(String[] args){
+        System.out.println("Hello World");
+    }
+}
