aaaa7f8b5ab8a7b6542643b01c099832e5eed0ee
GuoXuanKun <1925918787@qq.com>
2024-05-07 23:01:10 +0800

    Q13. 如何用 JavaScript 代码修改当前视频的播放倍速？

---
+++ src/day240507/practice/question/Q13.java
@@ -0,0 +1,8 @@
+package day240507.practice.question;
+
+//13. 如何用 JavaScript 代码修改当前视频的播放倍速？
+public class Q13 {
+    public static void main(String[] args) {
+        // document.getElementsByTagName('video')[0].playbackRate = 2
+    }
+}
