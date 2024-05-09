96b7037ed8f67a1f9fbd0b26857299f0fc51c7d8
GuoXuanKun <1925918787@qq.com>
2024-05-09 23:17:14 +0800

    final - 思考为什么当前状态的代码也会报错 (error)

--- src/day240509/practice/final_test_teacher/FinalVariable.java
+++ src/day240509/practice/final_test_teacher/FinalVariable.java
@@ -3,9 +3,9 @@ package day240509.practice.final_test_teacher;
 public class FinalVariable {
     // Variable 'id' might not have been initialized
     final int id;
-//    public FinalVariable() {
-//        id = 1;
-//    }
+    public FinalVariable() {
+        id = 1;
+    }
 //
 //    public FinalVariable(int id) {
 //        this.id = id;
