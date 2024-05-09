5a5c788bb07f8692909a0ae3405cb2f384e284ca
GuoXuanKun <1925918787@qq.com>
2024-05-09 15:09:46 +0800

    final rule : final parameters ...
    parameters Variable 参数 ...
    
    final 形参 给 局部 final 变量赋值

--- src/day240509/practice/final_test/FinalDemo.java
+++ src/day240509/practice/final_test/FinalDemo.java
@@ -1,13 +1,13 @@
 package day240509.practice.final_test;
 
-public class  FinalDemo {
+public class FinalDemo {
 //    Variable 'id' might not have been initialized
 //    变量“id”可能尚未初始化
 //    final int id;
 
 
-   // position 1
-    final int id  ;
+    // position 1
+    final int id;
 
 //    {
     // position 2
@@ -19,18 +19,20 @@ public class  FinalDemo {
         id = 1;
     }
 
-    public void test(){
+    public void test() {
         final int iu;
         iu = 1;
         System.out.println(iu);
     }
 
-    public void test1(final int id){
+    public void test1(final int id) {
 //        Cannot assign a value to final variable 'id'
 //        不能给最终变量“id”赋值
 //        id = 1;
-          int iu = id;
+        int iu = id;
         System.out.println(iu);
+        final int in = id;
+        System.out.println(in);
     }
 
 }
