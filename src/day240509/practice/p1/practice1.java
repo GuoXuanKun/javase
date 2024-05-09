f31c94a693f16bed9ac76f1c2e5cd52b07879b1d
GuoXuanKun <1925918787@qq.com>
2024-05-09 10:40:39 +0800

    "求同存异" : 养成 将思路写成代码的习惯

--- src/day240509/practice/p1/practice1.java
+++ src/day240509/practice/p1/practice1.java
@@ -2,10 +2,14 @@ package day240509.practice.p1;
 
 public class practice1 {
     public static void main(String[] args) {
+        // 开始：记录时间点
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
             sum += i;
         }
         System.out.println(sum);
+        // 结束：记录时间点
+        // 这个算法的运行时间 = 结束时间 - 开始时间
     }
-}
+    }
+
