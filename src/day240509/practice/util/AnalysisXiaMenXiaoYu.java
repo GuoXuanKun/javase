ea5a4432c3abb920b3cc99c6f4232f2c36cfd264
GuoXuanKun <1925918787@qq.com>
2024-05-09 16:55:36 +0800

    project : 解析不同的网站 -> step 8 ...
    更改 分析小鱼网的算法类中的 返回值

--- src/day240509/practice/util/AnalysisXiaMenXiaoYu.java
+++ src/day240509/practice/util/AnalysisXiaMenXiaoYu.java
@@ -4,6 +4,6 @@ public class AnalysisXiaMenXiaoYu extends AnalysisWebsite{
     @Override
     public String Analysis(String key) {
         // 先假设这里有具体分析算法
-        return key;
+        return "搜索完毕";
     }
 }
