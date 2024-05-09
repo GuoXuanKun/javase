3684bd802d31585457f3a060ce6609577bdfdf79
GuoXuanKun <1925918787@qq.com>
2024-05-09 10:41:45 +0800

    "求同存异" : 用 JDK 自带的工具获取算法执行之前和之后的时间点，计算差值

--- src/day240509/practice/p1/practice1.java
+++ src/day240509/practice/p1/practice1.java
@@ -3,6 +3,7 @@ package day240509.practice.p1;
 public class practice1 {
     public static void main(String[] args) {
         // 开始：记录时间点
+        long start = System.nanoTime();
         int sum = 0;
         for (int i = 1; i <= 100; i++) {
             sum += i;
@@ -10,6 +11,8 @@ public class practice1 {
         System.out.println(sum);
         // 结束：记录时间点
         // 这个算法的运行时间 = 结束时间 - 开始时间
+        long end = System.nanoTime();
+        System.out.println(end - start);
     }
     }
 
