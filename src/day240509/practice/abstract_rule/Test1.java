a0ac20608f4b962474c55909813ec85547158532
GuoXuanKun <1925918787@qq.com>
2024-05-09 11:51:35 +0800

    abstract rule : 不能修饰 final 类 ...
    //Illegal combination of modifiers: 'abstract' and 'final'
    //修饰语“abstract”和“final”的非法组合

--- src/day240509/practice/abstract_rule/Test1.java
+++ src/day240509/practice/abstract_rule/Test1.java
@@ -1,4 +1,6 @@
 package day240509.practice.abstract_rule;
 
-public final class Test1 {
+//Illegal combination of modifiers: 'abstract' and 'final'
+//修饰语“abstract”和“final”的非法组合
+public /*abstract*/ final class Test1 {
 }
