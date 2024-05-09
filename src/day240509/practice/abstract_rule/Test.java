c9d24b55c9fd0b81403db212a235874d02162730
GuoXuanKun <1925918787@qq.com>
2024-05-09 11:39:38 +0800

    abstract rule : 不能修饰静态方法 ...
    //    Illegal combination of modifiers: 'abstract' and 'static'
    //    修饰符` abstract `和` static `的非法组合

--- src/day240509/practice/abstract_rule/Test.java
+++ src/day240509/practice/abstract_rule/Test.java
@@ -13,4 +13,8 @@ public abstract class Test {
 //    Illegal combination of modifiers: 'abstract' and 'private'
 //    修饰语的非法组合:'abstract'和'private'
     /*abstract*/ private void test(){}
+
+//    Illegal combination of modifiers: 'abstract' and 'static'
+//    修饰符` abstract `和` static `的非法组合
+    public /*abstract*/ static void test1(){}
 }
