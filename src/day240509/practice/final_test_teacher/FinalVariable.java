7c6de36754d1651ed8db854fc86dacab98790aed
GuoXuanKun <1925918787@qq.com>
2024-05-09 23:10:59 +0800

    final - 修饰变量 Instance Variables (Non-Static Fields) 必须被初始化 - 在构造器中赋值 - 1

--- src/day240509/practice/final_test_teacher/FinalVariable.java
+++ src/day240509/practice/final_test_teacher/FinalVariable.java
@@ -2,5 +2,9 @@ package day240509.practice.final_test_teacher;
 
 public class FinalVariable {
     // Variable 'id' might not have been initialized
-    final int id = 1;
+    final int id;
+
+    public FinalVariable() {
+        id = 1;
+    }
 }
