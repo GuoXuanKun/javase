1f5747b437c07c6d321a71374eac2c238c345539
GuoXuanKun <1925918787@qq.com>
2024-05-09 16:47:25 +0800

    project : 解析不同的网站 -> step 6 ...
    完善分析网站工具类

--- src/day240509/practice/util/WebsideUtil.java
+++ src/day240509/practice/util/WebsideUtil.java
@@ -8,18 +8,22 @@ import java.io.IOException;
 
 // 分析网站的工具类
 public class WebsideUtil {
-    public static void util(String 网址 , 具体的分析算法,String 搜索的关键字) throws IOException {
+
+    //这个工具类方法需要传入 网址(String), 具体的分析算法(对象),要搜索的关键词(String)
+    public static void util(String url , AnalysisWebsite analysisWebsite,String key) throws Exception {
         // 把源码下载
-        Document doc = Jsoup.connect(网址).get();
+        Document doc = Jsoup.connect(url).get();
 
         // 解析算法
-        具体的分析算法
+        // 将要搜索的关键词传进算法 ,用 result 接收 分析结果
+        String result = analysisWebsite.Analysis(key);
+
 
         // 将解析的结果打印出来 或者 保存进数据库
-        System. out.println("解析的结果: ");
+        System. out.println("解析的结果: " + result);
 
 //         关键词命中、邮件发送
-        MyEmailUtil.send("xuankunguo@qq.com",解析的结果);
+        MyEmailUtil.send("xuankunguo@qq.com",result);
 }
 
 }
