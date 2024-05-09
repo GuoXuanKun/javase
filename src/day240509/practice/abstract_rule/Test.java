45a45ac937561c19f81a8993088a41de933aef3b
GuoXuanKun <1925918787@qq.com>
2024-05-09 11:27:32 +0800

    abstract rule : 不能修饰代码块

--- src/day240509/practice/abstract_rule/Test.java
+++ src/day240509/practice/abstract_rule/Test.java
@@ -3,4 +3,6 @@ package day240509.practice.abstract_rule;
 public abstract class Test {
 //    Modifier 'abstract' not allowed here
     /*abstract*/ int i;
+//    Modifier 'abstract' not allowed here
+    /*abstract*/ {};
 }
