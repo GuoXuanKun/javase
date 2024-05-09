0cadc4f5df15272cdc355c9cf9b0d8a9fe0bc11d
GuoXuanKun <1925918787@qq.com>
2024-05-09 10:38:43 +0800

    "求同存异" : 写出一个算法(累加1累加到100)

---
+++ src/day240509/practice/p1/practice1.java
@@ -0,0 +1,11 @@
+package day240509.practice.p1;
+
+public class practice1 {
+    public static void main(String[] args) {
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            sum += i;
+        }
+        System.out.println(sum);
+    }
+}
