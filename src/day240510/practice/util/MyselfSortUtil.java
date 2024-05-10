27a42d168822ea5814b67e3a9c94f5f51c3adbb9
GuoXuanKun <1925918787@qq.com>
2024-05-10 10:39:11 +0800

    project : MyselfSortUtil ...
    step 3 先写出支持能够 动态传入 double[] 的 冒泡排序排序

--- src/day240510/practice/util/MyselfSortUtil.java
+++ src/day240510/practice/util/MyselfSortUtil.java
@@ -1,12 +1,42 @@
 package day240510.practice.util;
 
 public class MyselfSortUtil {
-    //    int[] 冒泡排序
+    // int[] 冒泡排序
     public static int[] BubbleSort(int[] a) {
         int[] arr = a;
 
         //临时变量
         int temp;
+        // 遍历数组的长度
+        int length = arr.length - 1;
+
+        // 进行按位排序
+        for (int j = 0; j < arr.length; j++) {
+            for (int i = 0; i < length; i++) {
+                if (arr[i] >= arr[i + 1]) {
+                    temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                }
+
+            }
+            length -= 1;
+        }
+        //遍历数组
+        System.out.println("排序结果:");
+        for (int x = 0; x < arr.length; x++) {
+            System.out.print(arr[x] + " ");
+        }
+        System.out.println();
+        return arr;
+    }
+
+    // double[] 冒泡排序
+    public static double[] BubbleSort(double[] a){
+        double[] arr = a;
+
+        //临时变量
+        double temp;
         // 遍历数组的长度
         int length = arr.length - 1;
 
--- src/day240510/practice/util/Test.java
+++ src/day240510/practice/util/Test.java
@@ -2,7 +2,11 @@ package day240510.practice.util;
 
 public class Test {
     public static void main(String[] args) {
-        // 1.先做一个可以排序 int[] 的算法类
+        // 1.做一个可以排序 int[] 的算法类
         MyselfSortUtil.BubbleSort(new int[]{9, 5, 1, 6, 7});
+
+        // 2.做一个可以排序 double[] 的算法类
+        double[] student = new double[]{6.2,3.5,7.6,11.8,9.9};
+        MyselfSortUtil.BubbleSort(student);
     }
 }
