256bd48a2db2d6ab4dda7a750387fad9329e74e4
GuoXuanKun <1925918787@qq.com>
2024-05-11 22:41:54 +0800

    MySortUtil - 新增 Student 类型，创建出 Student[]

--- src/day240510/practice/teacher/Test.java
+++ src/day240510/practice/teacher/Test.java
@@ -6,5 +6,12 @@ public class Test {
         int[] intArr = new int[] {3, 5, 1, 2, 9};
         MySortUtil.sort(intArr);
         MySortUtil.print(intArr);
+
+        // 2. 支持 Student[]
+        Student s1 = new Student("小芳", 30);
+        Student s2 = new Student("小红", 45);
+        Student s3 = new Student("小张", 55);
+        Student[] studentArr = new Student[] {s1, s2, s3};
+
     }
 }
