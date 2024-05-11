915a49d17d8a18410463c86ec47240a82deffae3
GuoXuanKun <1925918787@qq.com>
2024-05-11 22:38:19 +0800

    MySortUtil - 轮廓

---
+++ src/day240510/practice/teacher/MySortUtil.java
@@ -0,0 +1,7 @@
+package day240510.practice.teacher;
+
+public class MySortUtil {
+    public static void sort(int[] arr) {
+        // ...
+    }
+}
---
+++ src/day240510/practice/teacher/Test.java
@@ -0,0 +1,9 @@
+package day240510.practice.teacher;
+
+public class Test {
+    public static void main(String[] args) {
+        // 1. 排序 int[]
+        int[] intArr = new int[] {3, 5, 1, 2, 9};
+        MySortUtil.sort(intArr);
+    }
+}
