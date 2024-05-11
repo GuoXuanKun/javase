4285333d14c076b2ed068c3fd862ff09513039d9
GuoXuanKun <1925918787@qq.com>
2024-05-11 22:39:03 +0800

    MySortUtil - 支持 int[]

--- src/day240510/practice/teacher/MySortUtil.java
+++ src/day240510/practice/teacher/MySortUtil.java
@@ -2,6 +2,19 @@ package day240510.practice.teacher;
 
 public class MySortUtil {
     public static void sort(int[] arr) {
-        // ...
+        for (int i = 0; i < arr.length - 1; i++) {
+            boolean swapped = false;
+            for (int j = 0; j < (arr.length - 1) - i; j++) {
+                if (arr[j] > arr[j+1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if (!swapped) {
+                break;
+            }
+        }
     }
 }
