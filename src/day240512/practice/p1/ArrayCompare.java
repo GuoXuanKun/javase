37f8c965d6fe35b9b6dd514084338af53533bbcc
GuoXuanKun <1925918787@qq.com>
2024-05-12 22:33:26 +0800

    解决自己的需求: 找出 两组数中 重复的数字   版本:1.1

--- src/day240512/practice/p1/ArrayCompare.java
+++ src/day240512/practice/p1/ArrayCompare.java
@@ -3,9 +3,11 @@ package day240512.practice.p1;
 import java.util.Scanner;
 
 public class ArrayCompare {
+
     public static void main(String[] args) {
-        int[] a = new int[25];
-        int[] b = new int[25];
+        int[] a = new int[100];
+        int[] b = new int[100];
+        int f = 0;
         Scanner scanner = new Scanner(System.in);
         System.out.println("请输入第一组数");
         for (int i = 0; i < a.length; i++) {
@@ -17,6 +19,7 @@ public class ArrayCompare {
                 a[i] = c;
             }
         }
+        System.out.println();
         System.out.println("请输入第二组数");
         for (int i = 0; i < b.length; i++) {
             System.out.println("请输入第 " + (i + 1) + " 个数");
@@ -31,9 +34,11 @@ public class ArrayCompare {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < b.length; j++) {
                 if (a[i] == b[j] && a[i] != 0) {
-                    System.out.println(a[i]);
+                    System.out.print(a[i]+ "   ");
+                    f += 1;
                 }
             }
         }
+        System.out.println("重复的数字有 " + f +" 个");
     }
 }
