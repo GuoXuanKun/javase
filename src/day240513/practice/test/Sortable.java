9547b7a7dc8cd30993dfadda88b0eb1f894dbccd
GuoXuanKun <1925918787@qq.com>
2024-05-13 11:10:30 +0800

    MySortUtil-2-该用 super(xxx) 的调用方式给父类的 xxx 赋值

--- src/day240513/practice/test/Sortable.java
+++ src/day240513/practice/test/Sortable.java
@@ -3,5 +3,12 @@ package day240513.practice.test;
 public class Sortable {
     // 变量类型 变量名 = 变量值;
     // `变量类型` 好像不太容易确定
-    double xxx;
+      double xxx;
+
+
+
+    // 方法1 设置一个提醒:需要对xxx进行赋值
+    public Sortable(double xxx) {
+        this.xxx = xxx;
+    }
 }
