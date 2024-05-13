5503b90565ab0829becc4ed03028fc98babea638
GuoXuanKun <1925918787@qq.com>
2024-05-13 17:31:12 +0800

    MySortUtil - 2 - 在子类重写的 isLargerThan 方法内，强转之前用 instanceof 进行判断，更安全

--- src/day240513/practice/test/TV.java
+++ src/day240513/practice/test/TV.java
@@ -23,11 +23,16 @@ public class TV extends Sortable {
 
     @Override
     public boolean isLargerThan(Sortable s) {
-        // 严格来说，这里要进行 instanceof 判断，确定了传进来的是 TV 类型，才能强制转成 TV
-        TV other = (TV) s;
-        if (this.price > other.price) {
-            return true;
+        if (s instanceof TV) {
+            TV other = (TV) s;
+            if (this.price > other.price) {
+                return true;
+            } else {
+                return false;
+            }
         } else {
+            // 后续学了异常之后，这里可以抛出异常
+            System.out.println("类型不匹配");
             return false;
         }
     }
