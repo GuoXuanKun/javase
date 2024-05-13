61f03ea41f25f12bc7eb7959adc7894e2460df8b
GuoXuanKun <1925918787@qq.com>
2024-05-13 16:46:24 +0800

    MySortUtil-2-子类必须主动调用父类有参的 super(xxx)，确保 xxx 必须有值，不能忘

--- src/day240513/practice/test/Dog.java
+++ src/day240513/practice/test/Dog.java
@@ -5,10 +5,11 @@ public class Dog extends Sortable {
     int age;
 
     public Dog(String name, int age) {
+        super(age);
         this.name = name;
         this.age = age;
         // 具体的子类中，哪个 state 要参与比较，就把它赋值给从父类基础的那个 xxx
-        this.xxx = this.age;
+//        this.xxx = this.age;
     }
 
     @Override
