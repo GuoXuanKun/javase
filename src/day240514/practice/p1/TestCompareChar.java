c2543ee4b3728095736a91e8520fa28e3d98d5bc
GuoXuanKun <1925918787@qq.com>
2024-05-14 15:36:15 +0800

    MySortUtil - 3 - TestCompareChar

---
+++ src/day240514/practice/p1/TestCompareChar.java
@@ -0,0 +1,15 @@
+package day240514.practice.p1;
+
+public class TestCompareChar {
+    public static void main(String[] args) {
+        char a = 'A';
+        char y = 'Y';
+        char z = 'Z';
+
+        System.out.println((int) a);
+        System.out.println((int) y);
+        System.out.println((int) z);
+
+        System.out.println(a > z);
+    }
+}
