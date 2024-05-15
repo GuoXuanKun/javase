68153afadea13cf748828077d11fa4680828c1c5
GuoXuanKun <1925918787@qq.com>
2024-05-15 16:55:00 +0800

    MySortUtil - 4 - 参考 Integer 里 通过构造器给 value 赋值的方式 ...
     ( 从Java 8版本开始，这个实体就被标记为过时. 这个实体在未来的版本中将被移除 )

--- src/day240515/practice/p2/p2/MyInt.java
+++ src/day240515/practice/p2/p2/MyInt.java
@@ -5,6 +5,10 @@ public class MyInt {
     private int value;
 
     //    Constructor
+
+    // since="8"就表示从Java 8版本开始，这个实体就被标记为过时。
+    // forRemoval = true表示这个实体在未来的版本中将被移除。
+    @Deprecated(since="8", forRemoval = true)
     public MyInt(int value) {
         this.value = value;
     }
