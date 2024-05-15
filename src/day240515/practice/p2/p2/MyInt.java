ee3f834e15113588d61a6205bf74b1068ca730a9
GuoXuanKun <1925918787@qq.com>
2024-05-15 16:45:21 +0800

    MySortUtil - 4 - 自己做一个 类 来包装 int

---
+++ src/day240515/practice/p2/p2/MyInt.java
@@ -0,0 +1,11 @@
+package day240515.practice.p2.p2;
+
+public class MyInt {
+//    state
+    private int value;
+
+//    Constructor
+    public MyInt(int value) {
+        this.value = value;
+    }
+}
