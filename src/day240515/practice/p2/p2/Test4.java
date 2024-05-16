4e7afd6e5d189f549452451f0a78231799e7fec2
GuoXuanKun <1925918787@qq.com>
2024-05-16 23:47:42 +0800

    MySortUtil - 4 - 基础数据类型的包装类

---
+++ src/day240515/practice/p2/p2/Test4.java
@@ -0,0 +1,17 @@
+package day240515.practice.p2.p2;
+
+public class Test4 {
+    public static void main(String[] args) {
+        // Byte
+        // Short
+        // int => Integer
+        // Long
+
+        // Float
+        // Double
+        // char => Character
+
+        // Boolean
+    }
+
+}
