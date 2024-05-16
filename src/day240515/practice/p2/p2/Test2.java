d1ee94333f7545afa5d1a0acb443995b0fe6a4b1
GuoXuanKun <1925918787@qq.com>
2024-05-16 23:30:18 +0800

    MySortUtil - 4 - 对 myIntArr ( int[] ) 进行排序

--- src/day240515/practice/p2/p2/Test2.java
+++ src/day240515/practice/p2/p2/Test2.java
@@ -6,5 +6,22 @@ public class Test2 {
         MyInt[] myIntArr = new MyInt[] {new MyInt(3), new MyInt(5), new MyInt(1), new MyInt(2), new MyInt(9)};
         // 让 int[] 可以传递到 print(Object[])
         MySortUtil.print(myIntArr);
+
+        MySortUtil.sort(myIntArr, new MyComparator() {
+            @Override
+            public int compare(Object o1, Object o2) {
+                MyInt t1 = (MyInt) o1;
+                MyInt t2 = (MyInt) o2;
+                if (t1.intValue() == t2.intValue()) {
+                    return 0;
+                } if (t1.intValue() > t2.intValue()) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            }
+        });
+
+        MySortUtil.print(myIntArr);
     }
 }
