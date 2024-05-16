85702069a7ab716b365f0bc75ca11d68356dfbc9
GuoXuanKun <1925918787@qq.com>
2024-05-16 23:26:32 +0800

    MySortUtil - 4 - 将int[] 包装进 MyInt

---
+++ src/day240515/practice/p2/p2/Test2.java
@@ -0,0 +1,8 @@
+package day240515.practice.p2.p2;
+
+public class Test2 {
+    public static void main(String[] args) {
+        //int[] intArr = new int[] {3, 5, 1, 2, 9};
+        MyInt[] myIntArr = new MyInt[] {new MyInt(3), new MyInt(5), new MyInt(1), new MyInt(2), new MyInt(9)};
+    }
+}
