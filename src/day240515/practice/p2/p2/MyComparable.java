50e9a75380266479e81f2cdd3f4ba4cac125f922
GuoXuanKun <1925918787@qq.com>
2024-05-17 20:22:40 +0800

    MySortUtil - 5 - 让 MyComparable 也支持范型

--- src/day240515/practice/p2/p2/MyComparable.java
+++ src/day240515/practice/p2/p2/MyComparable.java
@@ -1,19 +1,11 @@
 package day240515.practice.p2.p2;
 
-public interface MyComparable {
-    // 变量类型 变量名 = 变量值;
-    // `变量类型` 好像不太容易确定
-    /*final double xxx;
-
-    /*public Sortable(double xxx) {
-        this.xxx = xxx;
-    }*/
-
+public interface MyComparable<T> {
     /**
      * 这里只定义规范，具体的由各自子类实现
      *
-     * @param s MyComparable 类型
+     * @param s T 类型
      * @return int (0 代表相等；1 代表大于；-1 代表小于；-999 代表类型不匹配)
      */
-    int compareTo(MyComparable s);
+    int compareTo(T s);
 }
