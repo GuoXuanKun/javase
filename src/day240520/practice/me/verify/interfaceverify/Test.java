b5976e7e6172375b65b8e72872cf8852d76c9248
GuoXuanKun <1925918787@qq.com>
2024-05-20 22:37:13 +0800

    接口 Interface - 变量必须初始化 ...
    默认是 public static

---
+++ src/day240520/practice/me/verify/interfaceverify/Test.java
@@ -0,0 +1,9 @@
+package day240520.practice.me.verify.interfaceverify;
+
+public interface Test {
+    //    Variable 'value' might not have been initialized
+    // 变量“value”可能尚未初始化
+   public static int value = 0;
+
+
+}
