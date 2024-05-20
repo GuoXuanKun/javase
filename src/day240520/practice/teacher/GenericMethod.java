e5c799e8e4bacec60e3bbc19dce21bf8a59de95d
GuoXuanKun <1925918787@qq.com>
2024-05-20 22:01:57 +0800

    泛型 Generics - GenericMethod m1 的返回值也用 T

--- src/day240520/practice/teacher/GenericMethod.java
+++ src/day240520/practice/teacher/GenericMethod.java
@@ -1,13 +1,15 @@
 package day240520.practice.teacher;
 
 public class GenericMethod {
-    public <T> void m1(T t) {
-        System.out.println(t);
+    public <T> T m1(T t) {
+        return t;
     }
 
     public static void main(String[] args) {
         GenericMethod gm = new GenericMethod();
-        gm.m1(89);
-        gm.m1("string");
+        Integer i = gm.m1(89);
+        int i2 = gm.m1(89); // 这里也可以用 int 去接收它。参考 Wrapper class
+        String string = gm.m1("string");
     }
 }
+
