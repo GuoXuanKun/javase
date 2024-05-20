693190d2ab9a9a903b1a4ed8d62c02b7187f9237
GuoXuanKun <1925918787@qq.com>
2024-05-20 22:38:16 +0800

    接口 Interface - 方法 ...
    默认是 public abstract
    认 方法签名

--- src/day240520/practice/me/verify/interfaceverify/Test.java
+++ src/day240520/practice/me/verify/interfaceverify/Test.java
@@ -3,7 +3,9 @@ package day240520.practice.me.verify.interfaceverify;
 public interface Test {
     //    Variable 'value' might not have been initialized
     // 变量“value”可能尚未初始化
-   public static int value = 0;
+    int value = 0;
 
-
+    public abstract void test();
+    // 方法不能是静态的
+    public  void test1(int a);
 }
