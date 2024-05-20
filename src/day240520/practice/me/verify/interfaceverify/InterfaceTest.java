64833e27c49fca8ba3b31dbfddae25a36baa44fa
GuoXuanKun <1925918787@qq.com>
2024-05-20 22:43:26 +0800

    接口 Interface - 通过类实现 接口

--- src/day240520/practice/me/verify/interfaceverify/InterfaceTest.java
+++ src/day240520/practice/me/verify/interfaceverify/InterfaceTest.java
@@ -1,7 +1,17 @@
 package day240520.practice.me.verify.interfaceverify;
 
-public class InterfaceTest {
+public class InterfaceTest implements Test{
     public static void main(String[] args) {
-        Test test = new Test() ;
+//        Test test = new Test() ;
+    }
+
+    @Override
+    public void test() {
+
+    }
+
+    @Override
+    public void test1(int a) {
+
     }
 }
