06f71c3b76df60b3bb7ca1d6358be0438a57d02d
GuoXuanKun <1925918787@qq.com>
2024-05-20 17:00:13 +0800

    Wrapper class ( teacher 版)- ...
    Autoboxing - assignedToAVariable
    Autoboxing - passedAsAParameter

---
+++ src/day240520/practice/teacher/Boxing.java
@@ -0,0 +1,42 @@
+package day240520.practice.teacher;
+
+public class Boxing {
+    public static void main(String[] args) {
+        assignedToAVariable();
+        passedAsAParameter();
+    }
+
+    private static void assignedToAVariable() {
+        Byte referenceByte = (byte)1;
+        Short referenceShort = (short)2;
+        Integer referenceInteger = 3;
+        Long referenceLong = 4L;
+
+        Float referenceFloat = 5.0F;
+        Double referenceDouble = 6.0;
+
+        Character referenceCharacter = 'c';
+
+        Boolean referenceBoolean = false;
+    }
+
+    private static void passedAsAParameter() {
+        needReferenceByte((byte)1);
+        needReferenceShort((short)2);
+        needReferenceInteger(3);
+        needReferenceLong(4L);
+        needReferenceFloat(5.0F);
+        needReferenceDouble(6.0);
+        needReferenceCharacter('c');
+        needReferenceBoolean(true);
+    }
+
+    public static void needReferenceByte(Byte reference) {}
+    public static void needReferenceShort(Short reference) {}
+    public static void needReferenceInteger(Integer reference) {}
+    public static void needReferenceLong(Long reference) {}
+    public static void needReferenceFloat(Float reference) {}
+    public static void needReferenceDouble(Double reference) {}
+    public static void needReferenceCharacter(Character reference) {}
+    public static void needReferenceBoolean(Boolean reference) {}
+}
