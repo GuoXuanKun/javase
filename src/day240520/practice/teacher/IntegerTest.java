e8864941a874aa65ad378694be49d5b5e02423a7
GuoXuanKun <1925918787@qq.com>
2024-05-20 21:07:46 +0800

    包装类的核心学习思路

--- src/day240520/practice/teacher/IntegerTest.java
+++ src/day240520/practice/teacher/IntegerTest.java
@@ -4,5 +4,12 @@ public class IntegerTest {
     public static void main(String[] args) {
         System.out.println(Integer.MAX_VALUE);
         System.out.println(Integer.MIN_VALUE);
+
+        // 记住核心思路：
+        // 谁来帮我处理以下
+        // 谁.帮我处理以下
+        // XXX.xxx|xxx()
+
+        // 其他几个基础数据类也有对应的包装类，需要的时候用同样的思路去使用它
     }
 }
