26bf400395f43ad47733d0592039e7fcf2b272e7
GuoXuanKun <1925918787@qq.com>
2024-05-21 17:18:37 +0800

    泛型 Generics - 练习 - Group - 基础

---
+++ src/day240521/practice/teacher/group/Group.java
@@ -0,0 +1,11 @@
+package day240521.practice.teacher.group;
+
+public class Group {
+    String key;
+    String value;
+
+    public Group(String key, String value) {
+        this.key = key;
+        this.value = value;
+    }
+}
---
+++ src/day240521/practice/teacher/group/Test.java
@@ -0,0 +1,7 @@
+package day240521.practice.teacher.group;
+
+public class Test {
+    public static void main(String[] args) {
+        Group group1 = new Group("第一组", "4人");
+    }
+}
