0e0228236bba076f29fd0cf3c474d761e0943e98
GuoXuanKun <1925918787@qq.com>
2024-05-21 17:27:37 +0800

    泛型 Generics - 练习 - Group - 测试 <String, Integer>

--- src/day240521/practice/teacher/group/Test.java
+++ src/day240521/practice/teacher/group/Test.java
@@ -5,5 +5,15 @@ public class Test {
         Group<String, String> group1 = new Group<>("第一组", "4人");
         group1.set("第一组", "5人");
         System.out.println(group1.get("第一组"));
+
+        System.out.println("=== === ===");
+
+        m2();
+    }
+
+    private static void m2() {
+        Group<String, Integer> group1 = new Group<>("第一组", 4);
+        group1.set("第一组", 5);
+        System.out.println(group1.get("第一组"));
     }
 }
