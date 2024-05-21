802c1a25ca96eea408143ae1f2bd17834e38fbb9
GuoXuanKun <1925918787@qq.com>
2024-05-21 17:15:40 +0800

    泛型 Generics - 显式指定类型 int i2 = gm.<Integer>m1(89);

--- src/day240521/practice/teacher/generics/GenericMethod.java
+++ src/day240521/practice/teacher/generics/GenericMethod.java
@@ -8,8 +8,7 @@ public class GenericMethod {
     public static void main(String[] args) {
         GenericMethod gm = new GenericMethod();
         Integer i = gm.<Integer>m1(89);
-        int i2 = gm.<int>m1(89); // 这里也可以用 int 去接收它。参考 Wrapper class
+        int i2 = gm.<Integer>m1(89); // 这里也可以用 int 去接收它。参考 Wrapper class
         String string = gm.<String>m1("string");
     }
 }
-
