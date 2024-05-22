9136ffa7de5d694005bd6b11666d6d6779be1554
GuoXuanKun <1925918787@qq.com>
2024-05-22 22:29:15 +0800

    (teacher) String - 常用方法 - toUpperCase()

---
+++ src/day240522/practice/teacher/string/interview/StringCommonMethods.java
@@ -0,0 +1,10 @@
+package day240522.practice.teacher.string.interview;
+
+public class StringCommonMethods {
+    public static void main(String[] args) {
+        String code = "abcxyz";
+        System.out.println(code);
+        String codeUpperCase = code.toUpperCase();
+        System.out.println(codeUpperCase);
+    }
+}
