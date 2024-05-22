7f4f98d55ea104225c63c748df15a6fbaf7907d6
GuoXuanKun <1925918787@qq.com>
2024-05-22 22:11:47 +0800

    (teacher) 面试题 - String - Q2 - 字面量赋值 - 常量池...

---
+++ src/day240522/practice/teacher/string/interview/Q2.java
@@ -0,0 +1,9 @@
+package day240522.practice.teacher.string.interview;
+
+public class Q2 {
+    public static void main(String[] args) {
+        String s1 = "xyz";
+        String s2 = "xyz";
+        System.out.println(s1 == s2);
+    }
+}
