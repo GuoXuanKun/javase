3d7a9bb8ebf74a3a3d7f6243c0c86b4692b10241
GuoXuanKun <1925918787@qq.com>
2024-05-22 20:26:46 +0800

    (teacher) 面试题 - interface 接口 - InterfacesCannotBeInstantiated

---
+++ src/day240522/practice/teacher/interview1/InterfacesCannotBeInstantiated.java
@@ -0,0 +1,11 @@
+package day240522.practice.teacher.interview1;
+
+public interface InterfacesCannotBeInstantiated {
+}
+
+class Test {
+    public static void main(String[] args) {
+        // 'Xxx' is abstract; cannot be instantiated
+        //new InterfacesCannotBeInstantiated();
+    }
+}
