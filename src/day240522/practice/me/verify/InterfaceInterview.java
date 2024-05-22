431f6fe2621999fbccf9281b510e21269de543dd
GuoXuanKun <1925918787@qq.com>
2024-05-22 09:51:59 +0800

    接口 Interface - 验证 verify - 面试题 - 题目 ( error ) ...
        // Reference to 'size' is ambiguous, both 'B.size' and 'A.size' match
        // 对'size'的引用是模糊的，'B.size'和'A.size'都匹配

---
+++ src/day240522/practice/me/verify/InterfaceInterview.java
@@ -0,0 +1,27 @@
+package day240522.practice.me.verify;
+
+public class InterfaceInterview {
+    public static void main(String[] args) {
+
+    }
+}
+
+interface A {
+    int size = 5;
+}
+
+
+
+class B {
+    int size = 6;
+
+
+}
+
+class C extends B implements A {
+    // Reference to 'size' is ambiguous, both 'B.size' and 'A.size' match
+    // 对'size'的引用是模糊的，'B.size'和'A.size'都匹配
+    public void m() {
+        System.out.println(size);
+    }
+}
