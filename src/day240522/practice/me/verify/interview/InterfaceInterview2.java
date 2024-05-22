2bf8397923885f3ceb674bc3a4268cfa6daebf43
GuoXuanKun <1925918787@qq.com>
2024-05-22 11:59:15 +0800

    接口 Interface - 面试题 - fix ...
    两个 default Method 不能有 相同的 方法名
    或者 实现类 重写这个方法

--- src/day240522/practice/me/verify/interview/InterfaceInterview2.java
+++ src/day240522/practice/me/verify/interview/InterfaceInterview2.java
@@ -13,15 +13,12 @@ interface A {
 }
 
 interface B {
-    default void m() {
+    default void c() {
         System.out.println("B.m");
     }
 }
 
-/**
- * *day240522.practice.me.verify.interview.C inherits unrelated defaults for m() from types day240522.practice.me.verify.interview.A and day240522.practice.me.verify.interview.B
- * C从类型 A和 B继承了 m() 的不相关默认值
- */
+// 两个 default Method 不能有 相同的 方法名
 class C implements A, B {
 
 }
