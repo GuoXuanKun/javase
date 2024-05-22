18f6786970d316d572df3dca426ab4fee5b6214f
GuoXuanKun <1925918787@qq.com>
2024-05-22 20:24:18 +0800

    (teacher) 面试题 - interface 接口 - reference type

--- src/day240522/practice/teacher/interview1/InterfaceCommonForm.java
+++ src/day240522/practice/teacher/interview1/InterfaceCommonForm.java
@@ -2,6 +2,8 @@ package day240522.practice.teacher.interview1;
 
 /**
  * In its most common form, an interface is a group of related methods with empty bodies.
+ *
+ * In the Java programming language, an interface is a reference type
  */
 public interface InterfaceCommonForm {
     // Interface abstract methods cannot have body
