a778745905a173310f8fa26a35fd04fefa9fc743
GuoXuanKun <1925918787@qq.com>
2024-05-22 20:39:44 +0800

    (teacher) 面试题 - interface 接口 - ...
     have no braces and are terminated with a semicolon
     没有大括号并且以分号结束

--- src/day240522/practice/teacher/interview1/InterfacesCannotBeInstantiated.java
+++ src/day240522/practice/teacher/interview1/InterfacesCannotBeInstantiated.java
@@ -3,6 +3,11 @@ package day240522.practice.teacher.interview1;
 public interface InterfacesCannotBeInstantiated {
     // Not allowed in interface
     //public InterfacesCannotBeInstantiated() {}
+
+    // Note that the method signatures have no braces and are terminated with a semicolon.
+    // braces {}
+    // semicolon ;
+    void m1();
 }
 
 class Test {
