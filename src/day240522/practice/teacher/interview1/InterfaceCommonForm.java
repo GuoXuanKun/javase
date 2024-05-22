c9dabf6cd74954685d641a9efd31a457f3cdb8f8
GuoXuanKun <1925918787@qq.com>
2024-05-22 20:23:24 +0800

    (teacher) 面试题 - interface 接口 - most common form

---
+++ src/day240522/practice/teacher/interview1/InterfaceCommonForm.java
@@ -0,0 +1,10 @@
+package day240522.practice.teacher.interview1;
+
+/**
+ * In its most common form, an interface is a group of related methods with empty bodies.
+ */
+public interface InterfaceCommonForm {
+    // Interface abstract methods cannot have body
+    public abstract void m1();
+    void m2();
+}
