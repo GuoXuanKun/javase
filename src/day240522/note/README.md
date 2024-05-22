a6eda43537c88c606e5634ab1892ac3bb68442be
GuoXuanKun <1925918787@qq.com>
2024-05-22 22:14:39 +0800

    (teacher) 面试题 - String - Q2.class 对比 Q0.class 用 javap 命令显示出来的核心字节码差异

--- src/day240522/note/README.md
+++ src/day240522/note/README.md
@@ -38,3 +38,5 @@
 - ![img_7.png](img_7.png)
 - ![img_8.png](img_8.png)
 - ![img_9.png](img_9.png)
+- ![img_10.png](img_10.png)
+- ![img_11.png](img_11.png)
---
+++ src/day240522/practice/teacher/string/interview/Q0.java
@@ -0,0 +1,9 @@
+package day240522.practice.teacher.string.interview;
+
+public class Q0 {
+    public static void main(String[] args) {
+        String s1 = new String("xyz");
+        String s2 = new String("xyz");
+        System.out.println(s1 == s2);
+    }
+}
