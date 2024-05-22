a5a9b6deaa890dbb0d1ecf1a2213f55e85227afc
GuoXuanKun <1925918787@qq.com>
2024-05-22 09:59:20 +0800

    接口 Interface - 验证 verify - 面试题 - fix ...
     继承的类和实现的接口有同名变量，允许存在，但要精确访问

--- src/day240522/practice/me/verify/InterfaceInterview.java
+++ src/day240522/practice/me/verify/InterfaceInterview.java
@@ -22,6 +22,8 @@ class C extends B implements A {
     // Reference to 'size' is ambiguous, both 'B.size' and 'A.size' match
     // 对'size'的引用是模糊的，'B.size'和'A.size'都匹配
     public void m() {
-        System.out.println(size);
+        // 允许存在，只是要通过更精确的方式去访问它
+        System.out.println(A.size);
+        System.out.println(super.size);
     }
 }
