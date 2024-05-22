0270cee6cb876176493732793da0ef4bd9bf9a80
GuoXuanKun <1925918787@qq.com>
2024-05-22 22:30:20 +0800

    (teacher) String - 常用方法 - equalsIgnoreCase()

--- src/day240522/practice/teacher/string/interview/StringCommonMethods.java
+++ src/day240522/practice/teacher/string/interview/StringCommonMethods.java
@@ -4,7 +4,9 @@ public class StringCommonMethods {
     public static void main(String[] args) {
         String code = "abcxyz";
         System.out.println(code);
-        String codeUpperCase = code.toUpperCase();
+        String codeUpperCase = code.toUpperCase(); // ABCXYZ
         System.out.println(codeUpperCase);
+        System.out.println(code.equals(codeUpperCase));
+        System.out.println(code.equalsIgnoreCase(codeUpperCase));
     }
 }
