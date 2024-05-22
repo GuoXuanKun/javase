53b1e14bc23bc3511ecfc99aa6dc5cc0c265b525
GuoXuanKun <1925918787@qq.com>
2024-05-22 22:15:33 +0800

    (teacher) 面试题 - String - Q3 - 字符串字面量拼接及优化

---
+++ src/day240522/practice/teacher/string/interview/Q3.java
@@ -0,0 +1,23 @@
+package day240522.practice.teacher.string.interview;
+
+@SuppressWarnings("all")
+public class Q3 {
+    public static void main(String[] args) {
+        String s1 = "Java";
+        String s2 = "2403";
+
+        String s3 = "Java2403";
+
+        String s4 = "Java" + "2403";
+        String s5 = s1 + "2403";
+        String s6 = "Java" + s2;
+        String s7 = s1 + s2;
+
+        System.out.println(s3 == s4); // true
+        System.out.println(s3 == s5); // false
+        System.out.println(s3 == s6); // false
+        System.out.println(s3 == s7); // false
+        System.out.println(s5 == s6); // false
+        System.out.println(s6 == s7); // false
+    }
+}
