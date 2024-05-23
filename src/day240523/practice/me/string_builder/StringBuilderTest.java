9415d6f3004dfac0c5ef856788b908e6091de70e
GuoXuanKun <1925918787@qq.com>
2024-05-23 21:09:30 +0800

    String: 思考 为什么 StringBuilder.append() 可以一直拼接 , 数组不是定长的吗

---
+++ src/day240523/practice/me/string_builder/StringBuilderTest.java
@@ -0,0 +1,8 @@
+package day240523.practice.me.string_builder;
+
+public class StringBuilderTest {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("");
+    }
+}
