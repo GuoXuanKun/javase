a3eaa4d39edc9c247141eef99272db4a8bb38d53
GuoXuanKun <1925918787@qq.com>
2024-05-23 09:53:01 +0800

    "求同存异" : 新增支持计算具体的算法 Table99 的运行时间 ... 将计算时间的方法 : System.nanoTime() 改成 System.currentTimeMillis()

--- src/day240509/practice/p1/MyUtil.java
+++ src/day240509/practice/p1/MyUtil.java
@@ -3,11 +3,11 @@ package day240509.practice.p1;
 public class MyUtil {
     public static void calculateTime(Algorithm algorithm) {
         // 开始：记录时间点
-        long start = System.nanoTime();
+        long start = System.currentTimeMillis();
         algorithm.justDoIt();
         // 结束：记录时间点
         // 这个算法的运行时间 = 结束时间 - 开始时间
-        long end = System.nanoTime();
+        long end = System.currentTimeMillis();
         System.out.println(end - start);
     }
 }
