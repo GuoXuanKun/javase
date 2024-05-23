229e3cddecaaf67be87780e0fa1c83d80fbaca31
GuoXuanKun <1925918787@qq.com>
2024-05-23 22:52:03 +0800

    (teach) String : 字符串方法 - String 练习4：MyStringBuilder - StringBuilder 可能涉及的相关面试题

--- src/day240523/practice/teach/TestMyStringBuilder.java
+++ src/day240523/practice/teach/TestMyStringBuilder.java
@@ -4,5 +4,13 @@ public class TestMyStringBuilder {
     public static void main(String[] args) {
         MyStringBuilder msb = new MyStringBuilder();
         //msb.append("123");
+
+        // StringBuilder 可能涉及的相关面试题
+        // 0. 重点考察有没去阅读过源码，特别是大厂爱问源码
+        // 1. 底层是用什么类型存储数据的（>=JDK9 之后有变化）
+        // 2. 初始数组长度是多少？
+        // 3. 可以自己指定吗？怎么指定？
+        // 4. append 的时候超过数组长度，它是怎么处理的？
+        // ...
     }
 }
