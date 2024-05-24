fee9ba3bb5d4957997f370a5aba3e831ad421808
GuoXuanKun <1925918787@qq.com>
2024-05-24 16:50:45 +0800

    (teach) 源码 : 对比 StringBuilder 和 ArrayList 底层数组的初始长度

---
+++ src/day240524/practice/teach/NewStringBuilderVsArrayList.java
@@ -0,0 +1,13 @@
+package day240524.practice.teach;
+
+import java.util.ArrayList;
+
+/**
+ * 阅读源码练习之：对比 StringBuilder 和 ArrayList 底层数组的初始长度
+ */
+public class NewStringBuilderVsArrayList {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        ArrayList arrayList = new ArrayList();
+    }
+}
