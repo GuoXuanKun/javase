c81f6d01a454cff2777e58bf72f245cef7e9c5eb
GuoXuanKun <1925918787@qq.com>
2024-05-26 17:09:26 +0800

    命令行 "学生"管理系统 : step 3 - 将 Student 实例 存进 ArrayList

---
+++ src/day240526/practice/StudentManagementSystem.java
@@ -0,0 +1,30 @@
+package day240526.practice;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class StudentManagementSystem {
+    public static void main(String[] args) {
+        // 让用户输入基本信息
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("请输入学生学号");
+        int studentNumber = scanner.nextInt();
+        System.out.println("请输入学生姓名");
+        String name = scanner.next();
+        System.out.println("请输入学生的年龄");
+        int age = scanner.nextInt();
+        System.out.println("请输入学生的故乡");
+        String hometown = scanner.next();
+        Student student = new Student(studentNumber, name, age, hometown);
+
+        // 实例化 ArrayList 并将 Student 实例存进去
+        ArrayList<Student> studentList = new ArrayList<>();
+        studentList.add(student);
+
+        // 输出 ArrayList
+        for (int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList.get(i));
+        }
+
+    }
+}
