d555b9222463f954ab4ad33eafe0005592c0447b
GuoXuanKun <1925918787@qq.com>
2024-05-26 22:14:13 +0800

    命令行 "学生"管理系统 : step 21 - 增加 更新学生年龄的方法

--- src/day240526/practice/StudentManagementSystem.java
+++ src/day240526/practice/StudentManagementSystem.java
@@ -120,6 +120,26 @@ public class StudentManagementSystem {
         }
     }
 
+//    更新学生年龄
+public static void updateStudentAge(ArrayList<Student> list) {
+    Scanner scanner = new Scanner(System.in);
+    int signal = 0;
+    System.out.println("请输入要更新的学生的学号");
+    int oldNumber = scanner.nextInt();
+    for (int i = 0; i < list.size(); i++) {
+        if (list.get(i).getStudentNumber() == oldNumber) {
+            signal = 1;
+            System.out.println("请输入学生的新年龄");
+            int c = scanner.nextInt();
+            list.set(i, new Student(list.get(i).getStudentNumber(), list.get(i).getName(), c, list.get(i).getHometown()));
+            break;
+        }
+    }
+    if (signal == 0) {
+        System.out.println("查无此学生");
+    }
+}
+
     // 根据学号删除学生
     public static void deleteByStudentNumber(ArrayList<Student> list) {
         Scanner scanner = new Scanner(System.in);
