a06b7d037eea1669ad24c6f5405c7c3648d70724
GuoXuanKun <1925918787@qq.com>
2024-05-26 18:34:50 +0800

    命令行 "学生"管理系统 : step 6 - 查找特定的学生 ...
    通过 get() 方法

--- src/day240526/practice/StudentManagementSystem.java
+++ src/day240526/practice/StudentManagementSystem.java
@@ -30,6 +30,8 @@ public class StudentManagementSystem {
 
         // 查找特定的学生
         System.out.println("输入要查找第几位学生");
+        int findIndex = scanner.nextInt() - 1;
+        System.out.println(studentList.get(findIndex));
 
     }
 }
