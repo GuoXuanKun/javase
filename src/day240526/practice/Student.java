ab89a51325548aacc830f8925c98f11d9d60e365
GuoXuanKun <1925918787@qq.com>
2024-05-26 16:15:07 +0800

    命令行 "学生"管理系统 : step 1 - 建立 Student 类

---
+++ src/day240526/practice/Student.java
@@ -0,0 +1,18 @@
+package day240526.practice;
+
+public class Student {
+    // state
+    int studentNumber;
+    String  name;
+    int age;
+    String hometown;
+
+    // constractor
+
+    public Student(int studentNumber, String name, int age, String hometown) {
+        this.studentNumber = studentNumber;
+        this.name = name;
+        this.age = age;
+        this.hometown = hometown;
+    }
+}
