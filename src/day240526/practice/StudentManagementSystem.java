41c94637a463731b6ec2521f6ac91e6fb439f084
GuoXuanKun <1925918787@qq.com>
2024-05-26 20:25:49 +0800

    命令行 "学生"管理系统 : step 8 - 根据学号 更新学生的所有信息

--- src/day240526/practice/StudentManagementSystem.java
+++ src/day240526/practice/StudentManagementSystem.java
@@ -38,18 +38,50 @@ public class StudentManagementSystem {
 //        int findIndex = scanner.nextInt() - 1;
 //        System.out.println(studentList.get(findIndex));
 
-        // 根据学号删除学生
-        System.out.println("请输入要删除学生的学号");
-        int index = scanner.nextInt();
-        deleteByStudentNumber(studentList,index);
+//        // 根据学号删除学生
+//        System.out.println("请输入要删除学生的学号");
+//        int index = scanner.nextInt();
+//        deleteByStudentNumber(studentList,index);
+
+        // 根据学号更新学生信息
+        System.out.println("请输入要更新的学生的学号");
+        int old = scanner.nextInt();
+        updateStudentAll(studentList,old);
+        System.out.println("+------------------+------------+------------+------------+");
+        System.out.printf("| %-16s | %-10s | %-10s |%-10s |\n", "student number", "name", "age", "hometown");
+        for (int i = 0; i < studentList.size(); i++) {
+            System.out.println(studentList.get(i));
+        }
 
     }
-    public static void deleteByStudentNumber(ArrayList<Student> studentList,int studentNumber){
-        for (int i = 0; i < studentList.size(); i++) {
-            if (studentList.get(i).getStudentNumber() == studentNumber){
-                studentList.remove(i);
+    public static void deleteByStudentNumber(ArrayList<Student> list,int number){
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getStudentNumber() == number){
+                list.remove(i);
                 break;
             }
+        }
+    }
+    public static void updateStudentAll(ArrayList<Student> list,int oldNumber){
+        int signal = 0;
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getStudentNumber() == oldNumber){
+                signal = 1;
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("请输入学生的新学号");
+                int a = scanner.nextInt();
+                System.out.println("请输入学生的新名字");
+                String b = scanner.next();
+                System.out.println("请输入学生的新年龄");
+                int c = scanner.nextInt();
+                System.out.println("请输入学生的新故乡");
+                String d = scanner.next();
+                list.set(i,new Student(a,b,c,d));
+                break;
+            }
+        }
+        if (signal == 0){
+            System.out.println("查无此学生");
         }
     }
 }
