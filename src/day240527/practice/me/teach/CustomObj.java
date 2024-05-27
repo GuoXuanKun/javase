abf4066f52446b00e18e28c655e6d9e13c938574
GuoXuanKun <1925918787@qq.com>
2024-05-27 20:45:38 +0800

    Collection - Set - 如何判断是否重复 - CustomObj debug `equals() and hashCode()`

--- src/day240527/practice/me/teach/CustomObj.java
+++ src/day240527/practice/me/teach/CustomObj.java
@@ -29,6 +29,7 @@ public class CustomObj {
 
     @Override
     public int hashCode() {
+        // 可以在下一行打断点测试，同时也在 equals 中打断点。两元素的 hashCode 相同才会再通过 equals 进一步判断，以提高性能。
         return Objects.hash(id, name);
     }
 }
