d8b14464a8b73d30f3b1204b16e719065e760c05
GuoXuanKun <1925918787@qq.com>
2024-05-27 21:20:33 +0800

    Collection - Set - TreeSet - Xxx implements Comparable<Xxx>

--- src/day240527/practice/me/teach/Xxx.java
+++ src/day240527/practice/me/teach/Xxx.java
@@ -1,9 +1,21 @@
 package day240527.practice.me.teach;
 
-public class Xxx {
+public class Xxx implements Comparable<Xxx> {
     int value;
 
     public Xxx(int value) {
         this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Xxx{" +
+                "value=" + value +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Xxx o) {
+        return Integer.compare(this.value, o.value);
     }
 }
