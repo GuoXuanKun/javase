6009a7dded73d712a4b0e6d21e1fb5b07f716c5b
GuoXuanKun <1925918787@qq.com>
2024-05-27 22:54:45 +0800

    Map - Hashtable - Properties - setProperty(...)

--- src/day240527/practice/me/teach/PropertiesDemo.java
+++ src/day240527/practice/me/teach/PropertiesDemo.java
@@ -8,6 +8,8 @@ public class PropertiesDemo {
     public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
         properties.load(new FileInputStream("src/day240527/config.properties"));
+        properties.setProperty("username", "new-name");
+        properties.setProperty("password", "new-password");
         System.out.println(properties.get("username"));
         System.out.println(properties.get("password"));
     }
