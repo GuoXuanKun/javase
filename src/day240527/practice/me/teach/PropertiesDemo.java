12a9fb1b546d815acc0ef82d177353b1fc7acfb3
GuoXuanKun <1925918787@qq.com>
2024-05-27 22:57:00 +0800

    Map - Hashtable - Properties - store to file

--- src/day240527/practice/me/teach/PropertiesDemo.java
+++ src/day240527/practice/me/teach/PropertiesDemo.java
@@ -1,16 +1,20 @@
 package day240527.practice.me.teach;
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Properties;
 
 public class PropertiesDemo {
     public static void main(String[] args) throws IOException {
         Properties properties = new Properties();
-        properties.load(new FileInputStream("src/day240527/config.properties"));
+        String fileName = "src/day240527/config.properties";
+        properties.load(new FileInputStream(fileName));
         properties.setProperty("username", "new-name");
         properties.setProperty("password", "new-password");
         System.out.println(properties.get("username"));
         System.out.println(properties.get("password"));
+
+        properties.store(new FileOutputStream(fileName), properties.toString());
     }
 }
