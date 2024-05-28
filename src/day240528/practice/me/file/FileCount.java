22029800d66f8c62ba07abb5b4f822b62f411051
GuoXuanKun <1925918787@qq.com>
2024-05-28 23:14:28 +0800

    计算文件数量 - step3 - 写出大致思路

--- src/day240528/practice/me/file/FileCount.java
+++ src/day240528/practice/me/file/FileCount.java
@@ -10,5 +10,10 @@ public class FileCount {
         System.out.println(Arrays.toString(file.list()));
         // 这个方法打印出来的是下级文件数组
         System.out.println(Arrays.toString(file.listFiles()));
+
+        // 思路 : 想办法获取 文件夹的里面的文件
+        // 但是文件夹里面还嵌套这文件夹 利用 isDirectory() 判断 是不是文件夹
+        // 利用 listFiles() 层层 往下 直到 不是文件夹
+        // 猜测会不会有个方法比如 getfilename 可以 获取文件名
      }
 }
