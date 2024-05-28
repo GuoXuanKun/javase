0e38e3f1e1bd625ae9b175154c21e6ab07e1703d
GuoXuanKun <1925918787@qq.com>
2024-05-28 21:07:16 +0800

    (teach) Exceptions - Override - 子类重写方法抛出的异常大于父类 (error)

--- src/day240528/practice/teach/exceptions/OverrideWithExceptionSubclass.java
+++ src/day240528/practice/teach/exceptions/OverrideWithExceptionSubclass.java
@@ -2,10 +2,12 @@ package day240528.practice.teach.exceptions;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class OverrideWithExceptionSubclass extends OverrideWithExceptionSuper {
     @Override
-    public void m1() throws FileNotFoundException {
+    // 子类 Override 的方法，抛出的异常比父类更大，不行
+    public void m1() throws IOException {
         FileInputStream fileInputStream = new FileInputStream("config-prod.properties");
     }
 }
