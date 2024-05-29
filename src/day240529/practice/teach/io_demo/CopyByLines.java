bb4626872c3ed912b8c0a669de0c0618c4c4678c
GuoXuanKun <1925918787@qq.com>
2024-05-29 22:44:57 +0800

    (teach) IO - CopyByLines - BufferedReader + PrintWriter ...
    BufferedReader类用于从输入流中读取文本，缓冲字符，以提供字符、数组和行的高效读取
    PrintWriter类用于写入字符到输出流。作用类似于PrintStream，它的实例不产生异常，而是可选择的进行错误检查并提供诊断信息。
    
    PrintWriter有更强的格式化功能，如可以指定行的结束符，自动刷新缓冲区等。 PrintWriter 不仅可以输出文本，还可以输出字符数据、浮点数数据、整数数据等等。

--- src/day240529/practice/teach/io_demo/CopyByLines.java
+++ src/day240529/practice/teach/io_demo/CopyByLines.java
@@ -1,31 +1,29 @@
 package day240529.practice.teach.io_demo;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 
 public class CopyByLines {
     public static void main(String[] args) throws IOException {
 
         BufferedReader inputStream = null;
-        //PrintWriter outputStream = null;
+        PrintWriter outputStream = null;
 
         try {
-            inputStream = new BufferedReader(new FileReader("src/day240529/io_demo/from.txt"));
-            //outputStream = new PrintWriter(new FileWriter("characteroutput.txt"));
+            inputStream = new BufferedReader(new FileReader("src/day240529/practice/teach/io_demo/from.png"));
+            outputStream = new PrintWriter(new FileWriter("src/day240529/practice/teach/io_demo/to.png"));
 
             String l;
             while ((l = inputStream.readLine()) != null) {
-                System.out.println(l);
-                //outputStream.println(l);
+                //System.out.println(l);
+                outputStream.println(l);
             }
         } finally {
             if (inputStream != null) {
                 inputStream.close();
             }
-            /*if (outputStream != null) {
+            if (outputStream != null) {
                 outputStream.close();
-            }*/
+            }
         }
     }
 }
