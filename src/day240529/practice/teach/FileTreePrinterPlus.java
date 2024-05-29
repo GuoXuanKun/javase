2730eb9abd0de4e83830c7dd0e514c40c48fa7bb
GuoXuanKun <1925918787@qq.com>
2024-05-29 15:54:10 +0800

    (teach) File - FileTreePrinterPlus 文件树升级版

---
+++ src/day240529/practice/teach/FileTreePrinterPlus.java
@@ -0,0 +1,24 @@
+package day240529.practice.teach;
+
+import java.io.File;
+
+public class FileTreePrinterPlus {
+    public static void main(String[] args) {
+        File directory = new File("src/day240528");
+        print(directory, "");
+    }
+
+    public static void print(File file, String indent) {
+        System.out.println(indent + file.getName());
+
+        if (file.isDirectory()) {
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File f : files) {
+                    print(f, indent + "--");
+                }
+            }
+        }
+    }
+}
+
