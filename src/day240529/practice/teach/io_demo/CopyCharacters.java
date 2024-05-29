b24e9951d6e0d904f86878c1130503d38342616f
GuoXuanKun <1925918787@qq.com>
2024-05-29 17:29:31 +0800

    (teach) IO - CopyCharacters ...
    从一个文件复制到另一个文件
    每次读取一个字符

---
+++ src/day240529/practice/teach/io_demo/CopyCharacters.java
@@ -0,0 +1,30 @@
+package day240529.practice.teach.io_demo;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyCharacters {
+    public static void main(String[] args) throws IOException {
+
+        FileReader inputStream = null;
+        FileWriter outputStream = null;
+
+        try {
+            inputStream = new FileReader("src/day240529/io_demo/from.txt");
+            outputStream = new FileWriter("src/day240529/io_demo/to.txt");
+
+            int c;
+            while ((c = inputStream.read()) != -1) {
+                outputStream.write(c);
+            }
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        }
+    }
+}
