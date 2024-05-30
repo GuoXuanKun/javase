235abd1e0d6128309157a27fb9dedb67937cdc9f
GuoXuanKun <1925918787@qq.com>
2024-05-30 11:29:27 +0800

    (teach) URL - openConnection and why

---
+++ src/day240530/practice/teach/URLOpenConnection.java
@@ -0,0 +1,79 @@
+package day240530.practice.teach;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
+public class URLOpenConnection {
+    public static void main(String[] args) throws Exception {
+        //testBaidu();
+        //testBaiduTimeout();
+        //testCn();
+        //testCnWithCharset();
+        //testDouban();
+        //testDouban2();
+    }
+
+    static void testBaidu() throws Exception {
+        URL oracle = new URL("https://www.baidu.com/");
+        URLConnection uc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
+
+    static void testBaiduTimeout() throws Exception {
+        URL oracle = new URL("https://www.baidu.com/");
+        URLConnection uc = oracle.openConnection();
+        uc.setConnectTimeout(1);
+        //uc.setReadTimeout(1);
+        BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
+
+    static void testCn() throws Exception {
+        URL oracle = new URL("https://www.youth.cn");
+        URLConnection uc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
+
+    static void testCnWithCharset() throws Exception {
+        URL oracle = new URL("https://www.youth.cn");
+        URLConnection uc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream(), Charset.forName("gb2312"))); // gb2312
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
+
+    static void testDouban() throws Exception {
+        URL oracle = new URL("https://www.douban.com/");
+        URLConnection uc = oracle.openConnection();
+        BufferedReader in = new BufferedReader(new InputStreamReader(uc.getInputStream()));
+        String inputLine;
+        while ((inputLine = in.readLine()) != null) {
+            System.out.println(inputLine);
+        }
+        in.close();
+    }
+
+    static void testDouban2() throws Exception {
+        // 思考：如何破？
+    }
+}
