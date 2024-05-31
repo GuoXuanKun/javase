1c2c4c09e8625824ee0e36725f5cdb408c858003
GuoXuanKun <1925918787@qq.com>
2024-05-31 11:35:24 +0800

    (teach) IO - IOCommonFlow - try-catch-finally

--- src/day240531/practice/teach/IOCommonFlow.java
+++ src/day240531/practice/teach/IOCommonFlow.java
@@ -2,10 +2,11 @@ package day240531.practice.teach;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class IOCommonFlow {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         // 回顾之前的 `CopyBytes` 代码
         //FileInputStream in = new FileInputStream("src/day240531/io_demo/file.txt");
         // 可以传入 `文件路径` 给 FileInputStream 构造器
@@ -14,14 +15,31 @@ public class IOCommonFlow {
         // 所以，以`文件&输入流`为例子，它的通用流程是：
         // 1. new File，即：数据源（这里以文件为例，`流`的数据源也可以是其他，如：URL...）
         File file = new File("src/day240531/io_demo/file.txt");
-        // 2. 找一根 `管子` 接上去，即：`流`
-        FileInputStream fis = new FileInputStream(file);
-        // 3. 打开开关，让`东西`流出来
-        int c;
-        while ((c = fis.read()) != -1) {
-            System.out.println((char)c);
+        FileInputStream fis = null;
+        try {
+            // 2. 找一根 `管子` 接上去，即：`流`
+            fis = new FileInputStream(file);
+            // 3. 打开开关，让`东西`流出来
+            int c;
+            while ((c = fis.read()) != -1) {
+                System.out.println((char)c);
+            }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            throw new RuntimeException(e);
+        } finally {
+            // 4. 关闭 `流`
+            try {
+                if (fis != null) {
+                    fis.close();
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+                // 'throw' inside 'finally' block
+                throw new RuntimeException(e);
+            }
         }
-        // 4. 关闭 `流`
-        fis.close();
     }
 }
