c89987431840b8d0004b66ffdeeaac143a52d956
GuoXuanKun <1925918787@qq.com>
2024-05-31 11:55:38 +0800

    (teach) IO - IOCommonFlow - for to c - 添加文件

--- src/day240531/practice/teach/IOCommonFlow.java
+++ src/day240531/practice/teach/IOCommonFlow.java
@@ -8,13 +8,13 @@ import java.io.IOException;
 public class IOCommonFlow {
     public static void main(String[] args) {
         // 回顾之前的 `CopyBytes` 代码
-        //FileInputStream in = new FileInputStream("src/day240531/io_demo/file.txt");
+        //FileInputStream in = new FileInputStream("src/day240531/practice/teach");
         // 可以传入 `文件路径` 给 FileInputStream 构造器
         // 点进源码会发现，内部用这个 `文件路径` new 了 File 对象
 
         // 所以，以`文件&输入流`为例子，它的通用流程是：
         // 1. new File，即：数据源（这里以文件为例，`流`的数据源也可以是其他，如：URL...）
-        File file = new File("src/day240531/io_demo/file.txt");
+        File file = new File("src/day240531/practice/teach/file.txt");
         try (
                 // 2. 找一根 `管子` 接上去，即：`流`
                 FileInputStream fis = new FileInputStream(file)
@@ -26,6 +26,7 @@ public class IOCommonFlow {
                 for (int i = 0; i < c; i++) {
                     System.out.println((char) buffer[i]);
                 }
+                System.out.println("--- --- ---");
             }
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
