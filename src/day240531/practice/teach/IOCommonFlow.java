7fbc4360af143e21d763421e3f2478c7edc20e84
GuoXuanKun <1925918787@qq.com>
2024-05-31 11:12:49 +0800

    (teach) IO - IOCommonFlow - print -> println...
    为什么有两个空行？
    println 方法本身会插入空行，同时 `hello` 的下一个就是 `换行`（ASCII码表的 10）
    
    println 验证："写出来 + 换行" ...

---
+++ src/day240531/practice/teach/IOCommonFlow.java
@@ -0,0 +1,27 @@
+package day240531.practice.teach;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+public class IOCommonFlow {
+    public static void main(String[] args) throws IOException {
+        // 回顾之前的 `CopyBytes` 代码
+        //FileInputStream in = new FileInputStream("src/day240531/io_demo/file.txt");
+        // 可以传入 `文件路径` 给 FileInputStream 构造器
+        // 点进源码会发现，内部用这个 `文件路径` new 了 File 对象
+
+        // 所以，以`文件&输入流`为例子，它的通用流程是：
+        // 1. new File，即：数据源（这里以文件为例，`流`的数据源也可以是其他，如：URL...）
+        File file = new File("src/day240531/io_demo/file.txt");
+        // 2. 找一根 `管子` 接上去，即：`流`
+        FileInputStream fis = new FileInputStream(file);
+        // 3. 打开开关，让`东西`流出来
+        int c;
+        while ((c = fis.read()) != -1) {
+            System.out.println((char)c);
+        }
+        // 4. 关闭 `流`
+        fis.close();
+    }
+}
