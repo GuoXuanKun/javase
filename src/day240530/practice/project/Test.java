e30a42fd1655ae0785792ab4cf8a159539ce9c9e
GuoXuanKun <1925918787@qq.com>
2024-06-01 21:48:24 +0800

    Project : OOP-Project-V1 - 网站分析项目 - step 19 -  Jsoup 方式 分析模块的 类 JsoupParser - 进行测试

--- src/day240530/practice/project/Test.java
+++ src/day240530/practice/project/Test.java
@@ -1,10 +1,14 @@
 package day240530.practice.project;
 
 import day240530.practice.project.downloader.JsoupDownloader;
+import day240530.practice.project.parser.JsoupParser;
 
 public class Test {
     public static void main(String[] args) {
         JsoupDownloader jsoupDownloader = new JsoupDownloader();
         System.out.println(jsoupDownloader.download("http://bbs.xmfish.com/thread-htm-fid-55.html"));
+
+        JsoupParser jsoupParser = new JsoupParser();
+        System.out.println(jsoupParser.analysis(jsoupDownloader.download("http://bbs.xmfish.com/thread-htm-fid-55.html"), "苹果"));
     }
 }
