fc84a9661f2623a7d8716bba7351cd30f2a0ddc9
GuoXuanKun <1925918787@qq.com>
2024-06-02 21:12:32 +0800

    Project : OOP-Project-V1 - 网站分析项目 - step 36 - 在 WebsiteAnalysis 中 调用 EmailNotificatior 的匿名类的 notificator 进行发邮件

--- src/day240530/practice/project/WebsiteAnalysis.java
+++ src/day240530/practice/project/WebsiteAnalysis.java
@@ -2,6 +2,7 @@ package day240530.practice.project;
 
 import day240530.practice.project.downloader.JsoupDownloader;
 import day240530.practice.project.memorizer.PrintMemorizer;
+import day240530.practice.project.notificator.EmailNotificatior;
 import day240530.practice.project.parser.JsoupParser;
 import org.jsoup.nodes.Document;
 
@@ -9,12 +10,21 @@ import java.util.Scanner;
 
 public class WebsiteAnalysis {
     /*
-        下载模块:
-        通过配置文件 规定的 来执行相应的下载器
-        下载模块 目前 可以分为:
-        两种规范:
-               1. Jsoup
-               2. IO 流的规范
+        1. 下载模块:
+            通过 配置文件 来规定要执行的下载器
+            下载模块目前可以分为:
+            两种选择(未完待续):
+                    1. Jsoup
+                    2. IO 流的规范
+
+        2. 解析模块:
+            根据网站的不同通过 配置文件 来进行选择相应的下载器
+
+        3. 存储模块:
+                   通过 配置文件 来进行选择相应的存储器
+
+        4. 通知模块:
+                   通过 配置文件 来进行选择相应的通知器
         */
     public static void main(String[] args) {
         // 1. 下载模块
@@ -30,5 +40,10 @@ public class WebsiteAnalysis {
         // 3. 存储模块
         PrintMemorizer printMemorizer = new PrintMemorizer();
         printMemorizer.memorizer(content);
+
+        // 4. 通知模块
+        System.out.println("请输入要发送的邮件地址:");
+        String address = new Scanner(System.in).nextLine();
+        new EmailNotificatior().notificator(address,content);
     }
 }
