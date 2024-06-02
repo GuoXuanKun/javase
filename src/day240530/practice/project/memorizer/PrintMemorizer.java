40eb96066eb433607a343a098a11fe59b4ce8c2e
GuoXuanKun <1925918787@qq.com>
2024-06-02 16:53:14 +0800

    Project : OOP-Project-V1 - 网站分析项目 - step 24 - 实现 存储模块 的 方法类之一 - 直接打印出来

---
+++ src/day240530/practice/project/memorizer/PrintMemorizer.java
@@ -0,0 +1,8 @@
+package day240530.practice.project.memorizer;
+
+public class PrintMemorizer implements Memorizer{
+    @Override
+    public void memorizer(String string) {
+        System.out.println(string);
+    }
+}
