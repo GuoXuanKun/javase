fe0013a46f1f1db82f5532fb6ecfe8b8b3471f12
GuoXuanKun <1925918787@qq.com>
2024-06-03 17:38:10 +0800

    (teach) 阶段项目 - 网站解析 - 初始代码 - 先轮廓，再细节

---
+++ src/day240603/practice/teach/App.java
@@ -0,0 +1,9 @@
+package day240603.practice.teach;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("程序开始运行...");
+        // todo
+        System.out.println("程序结束运行");
+    }
+}
