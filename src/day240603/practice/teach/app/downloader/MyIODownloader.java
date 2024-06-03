8de55a3932c1c34099f4da8847c05fb2c0db7a64
GuoXuanKun <1925918787@qq.com>
2024-06-03 20:29:26 +0800

    (teach) 阶段项目 - 具体实现 MyIODownloader 的 download 方法

--- src/day240603/practice/teach/app/downloader/MyIODownloader.java
+++ src/day240603/practice/teach/app/downloader/MyIODownloader.java
@@ -2,10 +2,39 @@ package day240603.practice.teach.app.downloader;
 
 import day240603.practice.teach.app.downloader.Downloader;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
+import java.nio.charset.Charset;
+
 public class MyIODownloader extends Downloader {
     @Override
     public String download(String url) {
-        return "即将下载[" + url + "]的源码";
+        String result = "";
+        BufferedReader in = null;
+        try {
+            URL oracle = new URL(url);
+            URLConnection uc = oracle.openConnection();
+            in = new BufferedReader(new InputStreamReader(uc.getInputStream(), Charset.forName("gbk")));
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                result += inputLine;
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IO 异常");
+                throw new RuntimeException(e);
+            }
+        }
+        return result;
     }
 
     public MyIODownloader() {
