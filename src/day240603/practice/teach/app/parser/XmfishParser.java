b52f4cb48dac63f91c112ab2281cc2e19f2b37c2
GuoXuanKun <1925918787@qq.com>
2024-06-04 16:27:14 +0800

    (teach) 阶段项目 - 网站解析 - 补充 XmfishParser 解析逻辑

--- src/day240603/practice/teach/app/parser/XmfishParser.java
+++ src/day240603/practice/teach/app/parser/XmfishParser.java
@@ -2,6 +2,8 @@ package day240603.practice.teach.app.parser;
 
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,11 +12,23 @@ public class XmfishParser implements Parser {
     @Override
     public List<String> parse(String html) {
         Document document = Jsoup.parse(html);
-        // todo 解析
         List<String> titles = new ArrayList<>();
-        titles.add("苹果5折");
-        titles.add("xxx便宜转");
-        titles.add("新的奋斗奋斗");
+        Elements es = document.select("tbody[id=threadlist]").select("tr[class=tr3]");
+        for (Element e : es) {
+            // 过滤公告、置顶
+            Element noticeAndTop = e.select("td.icon a i").first();
+            if (noticeAndTop.hasAttr("alt") && "置顶帖标志".equals(noticeAndTop.attr("alt"))) {
+                continue;
+            }
+
+            Element titleElement = e.select("td.subject a.subject_t").first();
+            String title = titleElement.text();
+            titles.add(title);
+            //String url = titleElement.attr("href");
+            //url = "http://bbs.xmfish.com/" + url;
+            //String createdAt = e.select("td.author").first().select("p").text();
+        }
         return titles;
     }
 }
+
