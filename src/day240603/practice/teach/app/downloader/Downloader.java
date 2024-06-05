7d06a299d83c52f99002edc5710e4d76316d803e
GuoXuanKun <1925918787@qq.com>
2024-06-05 22:52:30 +0800

    (teach) 阶段项目 - 网站解析 - 反射 Reflection - get Downloader instance by MyReflectUtil.getInstance(...)

--- src/day240603/practice/teach/app/downloader/Downloader.java
+++ src/day240603/practice/teach/app/downloader/Downloader.java
@@ -1,5 +1,7 @@
 package day240603.practice.teach.app.downloader;
 
+import day240603.practice.teach.app.util.MyReflectUtil;
+
 import java.lang.reflect.Constructor;
 
 import static day240603.practice.teach.app.App.PROPERTIES;
@@ -7,16 +9,7 @@ import static day240603.practice.teach.app.App.PROPERTIES;
 public abstract class Downloader {
 
     public static Downloader getInstance() {
-        Downloader downloader;
-        try {
-            Class<?> downloaderClass = Class.forName(PROPERTIES.getProperty("downloader"));
-            Constructor<?> declaredConstructor = downloaderClass.getDeclaredConstructor();
-            downloader = (Downloader)declaredConstructor.newInstance();
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        return downloader;
+        return MyReflectUtil.getInstance(PROPERTIES.getProperty("downloader"));
     }
 
     public abstract String download(String url);
