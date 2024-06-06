7baa03e18665a7e6237911431d5f17b87cebd7c4
GuoXuanKun <1925918787@qq.com>
2024-06-06 17:51:36 +0800

    (teach) 单例 singleton - enum 版本 - 严格验证是否是同一个对象 - 方式三

--- src/day240606/practice/teach/singleton_demo/before/enum_verson/SingletonTest.java
+++ src/day240606/practice/teach/singleton_demo/before/enum_verson/SingletonTest.java
@@ -3,11 +3,7 @@ package day240606.practice.teach.singleton_demo.before.enum_verson;
 public class SingletonTest {
     public static void main(String[] args) {
         for (int i = 0; i < 5; i++) {
-            System.out.println(Student.INSTANCE);
+            System.out.println(Student.INSTANCE.hashCode());
         }
-
-        Student student1 = Student.INSTANCE;
-        Student student2 = Student.INSTANCE;
-        System.out.println(student1 == student2);
     }
 }
--- src/day240606/practice/teach/singleton_demo/before/enum_verson/Student.java
+++ src/day240606/practice/teach/singleton_demo/before/enum_verson/Student.java
@@ -2,9 +2,4 @@ package day240606.practice.teach.singleton_demo.before.enum_verson;
 
 public enum Student {
     INSTANCE;
-
-    @Override
-    public String toString() {
-        return getClass().getName() + "@" + Integer.toHexString(hashCode());
-    }
 }
