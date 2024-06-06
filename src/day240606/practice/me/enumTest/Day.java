b5aaf92d058788c55e3229d2621321a360d49662
GuoXuanKun <1925918787@qq.com>
2024-06-06 12:01:36 +0800

    枚举 - 用 类 写出类似枚举的功能

---
+++ src/day240606/practice/me/enumTest/Day.java
@@ -0,0 +1,14 @@
+package day240606.practice.me.enumTest;
+
+public class Day {
+    public static final  Day MONDAY = new Day();
+    public static final Day TUESDAY = new Day();
+    public static final Day WEDNESDAY = new Day();
+    public static final Day THURSDAY = new Day();
+    public static final Day FRIDAY = new Day();
+    public static final Day SATURDAY = new Day();
+    public static final Day SUNDAY = new Day();
+
+    private Day() {
+    }
+}
---
+++ src/day240606/practice/me/enumTest/DayTest.java
@@ -0,0 +1,7 @@
+package day240606.practice.me.enumTest;
+
+public class DayTest {
+    public static void main(String[] args) {
+        Day monday = Day.MONDAY;
+    }
+}
