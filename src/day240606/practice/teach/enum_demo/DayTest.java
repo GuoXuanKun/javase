bb8d7178834ae5ccd01617e2e708d2a0c1cd4451
GuoXuanKun <1925918787@qq.com>
2024-06-06 13:19:42 +0800

    (teach) 枚举 enum - Enum with switch

--- src/day240606/practice/teach/enum_demo/DayTest.java
+++ src/day240606/practice/teach/enum_demo/DayTest.java
@@ -5,6 +5,8 @@ import day240606.practice.teach.enum_demo.Day;
 public class DayTest {
     public static void main(String[] args) {
         whatExactlyIsIt();
+        System.out.println("===---===---===");
+        useWithSwitch(Day.TUESDAY);
     }
 
     private static void whatExactlyIsIt() {
@@ -18,6 +20,32 @@ public class DayTest {
         for (Day day : values) {
             System.out.println(day);
             System.out.println(day.name() + "-" + day.ordinal());
+        }
+    }
+
+    private static void useWithSwitch(Day day) {
+        switch (day) {
+            case MONDAY:
+                System.out.println("一脸懵");
+                break;
+            case TUESDAY:
+                System.out.println("前不着村，后不着店");
+                break;
+            case WEDNESDAY:
+                System.out.println("Midweek days are so-so.");
+                break;
+            case THURSDAY:
+                System.out.println("加把劲");
+                break;
+            case FRIDAY:
+                System.out.println("心已放假");
+                break;
+            case SATURDAY:
+                System.out.println("爽啊");
+                break;
+            case SUNDAY:
+                System.out.println("握草，这就...");
+                break;
         }
     }
 }
