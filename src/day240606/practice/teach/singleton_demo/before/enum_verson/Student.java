b0afc48e7f488b36bc6e450868ce619868dcc0de
GuoXuanKun <1925918787@qq.com>
2024-06-06 17:50:34 +0800

    (teach) 单例 singleton - enum 版本 - 严格验证是否是同一个对象 - 方式二

--- src/day240606/practice/teach/singleton_demo/before/enum_verson/Student.java
+++ src/day240606/practice/teach/singleton_demo/before/enum_verson/Student.java
@@ -2,4 +2,9 @@ package day240606.practice.teach.singleton_demo.before.enum_verson;
 
 public enum Student {
     INSTANCE;
+
+    @Override
+    public String toString() {
+        return getClass().getName() + "@" + Integer.toHexString(hashCode());
+    }
 }
