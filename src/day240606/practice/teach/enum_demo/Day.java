59a1036531ba66263e486cee97a7397304f1de0e
GuoXuanKun <1925918787@qq.com>
2024-06-06 13:23:43 +0800

    (teach) 枚举 enum - implements interface form2

--- src/day240606/practice/teach/enum_demo/Day.java
+++ src/day240606/practice/teach/enum_demo/Day.java
@@ -1,16 +1,46 @@
 package day240606.practice.teach.enum_demo;
 
 public enum Day implements ForEnumDemo {
-    MONDAY,
-    TUESDAY,
-    WEDNESDAY,
-    THURSDAY,
-    FRIDAY,
-    SATURDAY,
-    SUNDAY;
-
-    @Override
-    public void desc() {
-        System.out.println("Day.desc");
-    }
+    MONDAY {
+        @Override
+        public void desc() {
+            System.out.println("一脸懵");
+        }
+    },
+    TUESDAY {
+        @Override
+        public void desc() {
+            System.out.println("前不着村，后不着店");
+        }
+    },
+    WEDNESDAY {
+        @Override
+        public void desc() {
+            System.out.println("Midweek days are so-so.");
+        }
+    },
+    THURSDAY {
+        @Override
+        public void desc() {
+            System.out.println("加把劲");
+        }
+    },
+    FRIDAY {
+        @Override
+        public void desc() {
+            System.out.println("心已放假");
+        }
+    },
+    SATURDAY {
+        @Override
+        public void desc() {
+            System.out.println("爽啊");
+        }
+    },
+    SUNDAY {
+        @Override
+        public void desc() {
+            System.out.println("握草，这就...");
+        }
+    };
 }
