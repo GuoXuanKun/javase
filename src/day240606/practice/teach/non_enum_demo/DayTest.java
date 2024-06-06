be4c8b10bd26bcbc015845fd45fedc321b2c7db8
GuoXuanKun <1925918787@qq.com>
2024-06-06 13:16:42 +0800

    (teach) 枚举 enum vs non-enum ...
    用类实现 枚举的功能

---
+++ src/day240606/practice/teach/non_enum_demo/DayTest.java
@@ -0,0 +1,8 @@
+package day240606.practice.teach.non_enum_demo;
+
+public class DayTest {
+    public static void main(String[] args) {
+        Day thursday = Day.THURSDAY;
+        System.out.println(thursday);
+    }
+}
