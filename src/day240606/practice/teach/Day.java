e6ba181a4fdb846c94f28bc2a0310232ee86d243
GuoXuanKun <1925918787@qq.com>
2024-06-06 13:14:17 +0800

    (teach) 枚举 enum - intro

---
+++ src/day240606/practice/teach/Day.java
@@ -0,0 +1,11 @@
+package day240606.practice.teach;
+
+public enum Day {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
+}
---
+++ src/day240606/practice/teach/DayTest.java
@@ -0,0 +1,12 @@
+package day240606.practice.teach;
+
+public class DayTest {
+    public static void main(String[] args) {
+        whatExactlyIsIt();
+    }
+
+    private static void whatExactlyIsIt() {
+        Day thursday = Day.THURSDAY;
+        System.out.println(thursday);
+    }
+}
