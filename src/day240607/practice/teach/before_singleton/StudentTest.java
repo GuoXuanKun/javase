d92e0e3fe527e2179b4eda10a1121f893152558e
GuoXuanKun <1925918787@qq.com>
2024-06-08 21:41:16 +0800

    (teach) 单例练习：step2 - Class new 出 Object

--- src/day240607/practice/teach/before_singleton/StudentTest.java
+++ src/day240607/practice/teach/before_singleton/StudentTest.java
@@ -2,6 +2,9 @@ package day240607.practice.teach.before_singleton;
 
 public class StudentTest {
     public static void main(String[] args) {
-
+        // 变量
+        // 变量类型 变量名 = 变量值;
+        Student student1 = new Student();
+        System.out.println(student1);
     }
 }
