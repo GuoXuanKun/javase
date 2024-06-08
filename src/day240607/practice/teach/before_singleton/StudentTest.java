3bc540547be7009a260a86d8de3043b4ffb70e41
GuoXuanKun <1925918787@qq.com>
2024-06-08 21:42:49 +0800

    (teach) 单例练习：step3 自动生成的构造器...
    - 如果没有自己写构造器，默认会自动生成一个空参的构造器
    - new Student(); 的本质就是调用构造器

--- src/day240607/practice/teach/before_singleton/StudentTest.java
+++ src/day240607/practice/teach/before_singleton/StudentTest.java
@@ -4,6 +4,10 @@ public class StudentTest {
     public static void main(String[] args) {
         // 变量
         // 变量类型 变量名 = 变量值;
+        // new Student() 这条语句本质上调用什么？
+        //   - 构造器
+        //   - 看 out 目录自动编译出来的 class 文件
+        //   - 能看到在 class 文件中自动生成了 构造器
         Student student1 = new Student();
         System.out.println(student1);
     }
