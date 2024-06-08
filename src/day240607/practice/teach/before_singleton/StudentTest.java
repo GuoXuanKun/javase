edb2a277bd5857c56da5e8af405213b0661f43ec
GuoXuanKun <1925918787@qq.com>
2024-06-08 21:46:53 +0800

    (teach) 单例练习：step8 - 注释代码，解决报错，同时在注释中记录下一步该做什么

--- src/day240607/practice/teach/before_singleton/StudentTest.java
+++ src/day240607/practice/teach/before_singleton/StudentTest.java
@@ -8,13 +8,15 @@ public class StudentTest {
         //   - 构造器
         //   - 看 out 目录自动编译出来的 class 文件
         //   - 能看到在 class 文件中自动生成了 构造器
-        Student student1 = new Student();
-        System.out.println(student1);
+//        Student student1 = new Student();
+//        System.out.println(student1);
+//
+//        Student student2 = new Student();
+//        System.out.println(student2);
+//
+//        Student student3 = new Student();
+//        System.out.println(student3);
 
-        Student student2 = new Student();
-        System.out.println(student2);
-
-        Student student3 = new Student();
-        System.out.println(student3);
+        // 思考：现在外面不能直接 new 它，那可以在哪里 new ?
     }
 }
