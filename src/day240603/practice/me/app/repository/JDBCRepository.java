4460ba296397a99b5b3618d5d2530dbccb8b18a5
GuoXuanKun <1925918787@qq.com>
2024-06-13 22:49:43 +0800

    阶段项目 - 网站解析 - 增加数据库存储方式 - 完善 JDBCRepository

--- src/day240603/practice/me/app/repository/JDBCRepository.java
+++ src/day240603/practice/me/app/repository/JDBCRepository.java
@@ -4,6 +4,7 @@ import day240603.practice.me.app.dto.CustomResult;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
@@ -12,5 +13,15 @@ public class JDBCRepository implements Repository {
     @Override
     public void store(List<CustomResult> results) throws SQLException {
         Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb1", "root", "这里放密码");
+
+        String insertSql = "insert into article(title,url,create_at,update_at) values(?, ?, ?, ?);";
+        PreparedStatement ppstmt = connection.prepareStatement(insertSql);
+        for (CustomResult cr : results) {
+            ppstmt.setString(1, cr.getTitle());
+            ppstmt.setString(2, cr.getUrl());
+            ppstmt.setString(3, cr.getCreatedAt());
+            ppstmt.setString(4, cr.getUpdatedAt());
+            ppstmt.executeUpdate();
+        }
     }
 }
