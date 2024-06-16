d2b3668e4ebd0e6251447ebae92a918502a8e9e0
GuoXuanKun <1925918787@qq.com>
2024-06-16 21:56:34 +0800

    阶段项目 - 网站解析 - 将 JDBC 专门做成一个工具方法 - 完善 JDBCUtil

--- src/day240603/practice/me/app/util/JDBCUtil.java
+++ src/day240603/practice/me/app/util/JDBCUtil.java
@@ -1,4 +1,16 @@
 package day240603.practice.me.app.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCUtil {
+    static Connection connection;
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mydb1", "root", "这里放密码");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
